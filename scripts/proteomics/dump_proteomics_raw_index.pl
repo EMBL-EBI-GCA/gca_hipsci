518d1fc46c83f0bf5cc153f5757ab5c09e8fbc69
resequencing informatics <rseqpipe@ebi-005.ebi.ac.uk>
2014-06-06 12:51:47 +0100

    bugfix

--- scripts/proteomics/dump_proteomics_raw_index.pl
+++ scripts/proteomics/dump_proteomics_raw_index.pl
@@ -48,7 +48,7 @@ foreach my $file (@{$fa->fetch_by_type($type)}) {
 }
 
 
-print '#', join("\t", qw(cell_line raw_id fractionation)), "\n"
+print '#', join("\t", qw(cell_line raw_id fractionation)), "\n";
 foreach my $cell_line (sort {$a cmp $b} keys %cell_line_raw) {
   foreach my $raw_id (@{$cell_line_raw{$cell_line}}) {
     my $frac_method = $num_raw{$raw_id} == 16 ? 'SAX'
