576002ea3a8ac1a7127cd6ce166f37b0ddb10634
resequencing informatics <rseqpipe@ebi-005.ebi.ac.uk>
2014-06-09 17:24:58 +0100

    bash errors corrected

--- bash/private_track/dump_proteomics_maxquant_index.bash
+++ bash/private_track/dump_proteomics_maxquant_index.bash
@@ -6,7 +6,7 @@ perl $HIPSCI_CODE/scripts/proteomics/dump_proteomics_maxquant_index.pl \
   -dbuser g1kro -dbport 4197 -dbname hipsci_private_track -dbhost mysql-g1kdcc-public \
   -type PROT_TXT \
   > proteomics.maxquant.$datestamp.index \
-&& mv -f proteomics.maxquant.index /nfs/research2/hipsci/drop/hip-drop/tracked/proteomics \
+&& mv -f proteomics.maxquant.$datestamp.index /nfs/research2/hipsci/drop/hip-drop/tracked/proteomics \
 && perl $RESEQTRACK/scripts/file/load_files.pl  \
   -dbuser g1krw -dbport 4197 -dbpass $RESEQTRACK_PASS -dbname hipsci_private_track -dbhost mysql-g1kdcc-public \
   -file /nfs/research2/hipsci/drop/hip-drop/tracked/proteomics/proteomics.maxquant.$datestamp.index \
