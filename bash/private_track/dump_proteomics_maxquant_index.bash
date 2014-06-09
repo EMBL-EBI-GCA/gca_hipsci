1fad6e98db47a4d6e973de8beec7b622d2091806
istreeter <streeter@ebi.ac.uk>
2014-06-09 17:12:49 +0100

    Proteomics index files to have date

--- bash/private_track/dump_proteomics_maxquant_index.bash
+++ bash/private_track/dump_proteomics_maxquant_index.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+datestamp=`date +%Y%m%d`
+
 perl $HIPSCI_CODE/scripts/proteomics/dump_proteomics_maxquant_index.pl \
   -dbuser g1kro -dbport 4197 -dbname hipsci_private_track -dbhost mysql-g1kdcc-public \
   -type PROT_TXT \
-  > proteomics.maxquant.index \
-&& mv -f proteomics.maxquant.index /nfs/research2/hipsci/drop/hip-drop/tracked/proteomics/maxquant \
+  > proteomics.maxquant.$datestamp.index \
+&& mv -f proteomics.maxquant.index /nfs/research2/hipsci/drop/hip-drop/tracked/proteomics \
 && perl $RESEQTRACK/scripts/file/load_files.pl  \
   -dbuser g1krw -dbport 4197 -dbpass $RESEQTRACK_PASS -dbname hipsci_private_track -dbhost mysql-g1kdcc-public \
-  -file /nfs/research2/hipsci/drop/hip-drop/tracked/proteomics/maxquant/proteomics.maxquant.index \
+  -file /nfs/research2/hipsci/drop/hip-drop/tracked/proteomics/proteomics.maxquant.$datestamp.index \
   -host 1000genomes.ebi.ac.uk \
   -run \
   -update \
