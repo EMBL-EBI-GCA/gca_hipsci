d385521d4d4aff91a198a7537ed891367d3dbcda
streeter <streeter@ebi.ac.uk>
2014-08-21 14:22:17 +0100

    HipSci current tree to use ReseqTrack script

--- bash/public_track/dump_tree.bash
+++ bash/public_track/dump_tree.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-staging=/nfs/1000g-work/hipsci/archive_staging/ftp
-
 perl $RESEQTRACK/scripts/file/run_tree_for_ftp.pl \
   -dbname hipsci_track -dbhost mysql-g1kdcc-public -dbuser g1krw -dbport 4197 -dbpass $RESEQTRACK_PASS \
+  -skip_load -skip_archive \
   -dir_to_tree /nfs/hipsci/vol1/ftp \
-  -staging_dir $staging \
-  -log_dir $PWD \
+  -staging_dir /nfs/1000g-work/hipsci/archive_staging/ftp \
+  -log_dir $PWD/logging_dir
