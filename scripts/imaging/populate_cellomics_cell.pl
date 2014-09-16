ed38b058d9eb8c652eab331413b6d225cb58ee15
istreeter <streeter@ebi.ac.uk>
2014-09-16 17:14:57 +0100

    much faster loading of the cellomics cell_line table

--- scripts/imaging/populate_cellomics_cell.pl
+++ scripts/imaging/populate_cellomics_cell.pl
@@ -37,32 +37,45 @@ my $sql1 = <<"SQL";
     total_inten, avg_inten, var_inten
   )
   VALUES (
-      (SELECT experiment_id FROM experiment WHERE barcode = ? AND w_field_id = ? AND channel = ?),
-      ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+      ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
   )
 SQL
+my $sql2 = 'SELECT experiment_id, w_field_id, channel FROM experiment WHERE barcode = ?';
 my $sth1 = $dbh->prepare($sql1);
+my $sth2 = $dbh->prepare($sql2);
+
+my %experiment_ids;
 
 my $cell_file = new Text::Delimited;
 $cell_file->delimiter("\t");
 $cell_file->open($file) or die "could not open $file $!";
 LINE:
 while (my $line_data = $cell_file->read) {
-  $sth1->bind_param(1, $line_data->{'barcode'}),
-  $sth1->bind_param(2, $line_data->{'wFieldID'}),
-  $sth1->bind_param(3, $line_data->{'channel'}),
-  $sth1->bind_param(4, $line_data->{'XCentroid'}),
-  $sth1->bind_param(5, $line_data->{'YCentroid'}),
-  $sth1->bind_param(6, $line_data->{'Left'}),
-  $sth1->bind_param(7, $line_data->{'Top'}),
-  $sth1->bind_param(8, $line_data->{'Height'}),
-  $sth1->bind_param(9, $line_data->{'Width'}),
-  $sth1->bind_param(10, $line_data->{'Area'}),
-  $sth1->bind_param(11, $line_data->{'ShapeP2A'}),
-  $sth1->bind_param(12, $line_data->{'ShapeLWR'}),
-  $sth1->bind_param(13, $line_data->{'TotalInten'}),
-  $sth1->bind_param(14, $line_data->{'AvgInten'}),
-  $sth1->bind_param(15, $line_data->{'VarInten'}),
+  if (!$experiment_ids{$line_data->{'barcode'}}) {
+    $sth2->bind_param(1, $line_data->{'barcode'}),
+    $sth2->execute or die "could not process ".$line_data->{'__LINE__'};
+    while (my $row = $sth2->fetchrow_arrayref) {
+      $experiment_ids{$line_data->{'barcode'}}{$row->[2]}{$row->[1]} = $row->[0];
+    }
+  }
+  my $experiment_id = $experiment_ids{$line_data->{'barcode'}}{$line_data->{'channel'}}{$line_data->{'wFieldID'}};
+  if (!$experiment_id) {
+    throw( "do not have experiment_id for ".$line_data->{'__LINE__'});
+  }
+
+  $sth1->bind_param(1, $experiment_id);
+  $sth1->bind_param(2, $line_data->{'XCentroid'}),
+  $sth1->bind_param(3, $line_data->{'YCentroid'}),
+  $sth1->bind_param(4, $line_data->{'Left'}),
+  $sth1->bind_param(5, $line_data->{'Top'}),
+  $sth1->bind_param(6, $line_data->{'Height'}),
+  $sth1->bind_param(7, $line_data->{'Width'}),
+  $sth1->bind_param(8, $line_data->{'Area'}),
+  $sth1->bind_param(9, $line_data->{'ShapeP2A'}),
+  $sth1->bind_param(10, $line_data->{'ShapeLWR'}),
+  $sth1->bind_param(11, $line_data->{'TotalInten'}),
+  $sth1->bind_param(12, $line_data->{'AvgInten'}),
+  $sth1->bind_param(13, $line_data->{'VarInten'}),
   $sth1->execute or die "could not process ".$line_data->{'__LINE__'};
 }
 $cell_file->close;
