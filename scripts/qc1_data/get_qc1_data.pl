aab7fdf9d851b5df56c303b7c371a5bba4c06294
istreeter <streeter@ebi.ac.uk>
2015-01-12 18:13:27 +0000

    modified qc1 data retrieval

--- scripts/qc1_data/get_qc1_data.pl
+++ scripts/qc1_data/get_qc1_data.pl
@@ -4,22 +4,31 @@ use strict;
 use warnings;
 
 use Net::OpenSSH;
+use Net::SFTP::Foreign;
 use LWP::UserAgent;
 use JSON;
-use List::Util;
+use List::Util qw();
 use Getopt::Long;
+use File::Basename qw(fileparse);
+use POSIX qw(strftime);
+use ReseqTrack::Tools::FileSystemUtils qw(check_directory_exists);
 
 my $ssh_password;
 my $ssh_user = 'is6';
 my $ssh_host = 'ssh.sanger.ac.uk';
 my $output = 'hipsci.qc1';
+my $filesystem_password;
+my $plot_directory;
 GetOptions('ssh_user=s' => \$ssh_user,
           'ssh_host=s' => \$ssh_host,
           'ssh_password=s' => \$ssh_password,
           'output=s' => \$output,
+          'filesystem_password=s' => \$filesystem_password,
+          'plot_directory=s' => \$plot_directory,
           );
 
 my $ssh = Net::OpenSSH->new($ssh_host, user => $ssh_user, password=>$ssh_password);
+my $sftp = Net::SFTP::Foreign->new(host => 'localhost', user => $ssh_user, port => 9091, password => $filesystem_password);
 
 my $ua = LWP::UserAgent->new(ssl_opts => {verify_hostname => 0});
 $ua->default_header('Content-Type' => 'application/json');
@@ -62,14 +71,19 @@ print $loh_fh join("\t", qw(control_sample sample chromosome start end count)), 
 
 DONOR:
 foreach my $donor_id (map {$_->{id}} @$donors) {
-  $donor_id = 8062;
   $donor_id += 0; # forces it to be treated as a number in the json encoding
   $response = $ua->post('https://localhost:9090/rest/qc/donor_qc',
       Content => $json->encode({donor => $donor_id}),
     );
   my $donor_qc = $json->decode($response->content);
+
+  my $donor_name;
+
   foreach my $gender_qc (grep {$_->{type} eq 'gender'} @$donor_qc) {
+    next DONOR if !$gender_qc->{sample_public_name};
     print $gender_fh join("\t", @{$gender_qc}{qw(sample_public_name sample_name expected_gender actual_gender)}), "\n";
+    my ($donor_name_part1, $donor_name_part2) = $gender_qc->{sample_public_name} =~ /^(\w+\d+)\w+-([a-z]+)/;
+    $donor_name = $donor_name_part1 . '-' . $donor_name_part2;
   }
   foreach my $discordance_qc (grep {$_->{type} eq 'discordance_genotyping'} @$donor_qc) {
     print $discordance_genotyping_fh join("\t", @{$discordance_qc}{qw(sample1_public_name sample2_public_name sample1_control sample2_control discordance num_of_sites avg_min_depth)}), "\n";
@@ -85,6 +99,17 @@ foreach my $donor_id (map {$_->{id}} @$donors) {
   }
   foreach my $cnv_qc (grep {$_->{type} eq 'aberrant_regions'} @$donor_qc) {
     print $cnv_aberrant_region_fh join("\t", @{$cnv_qc}{qw(sample cn chr start end length quality)}), "\n";
+    my $remote_file = $cnv_qc->{graph};
+    my ($remote_name, $remote_dir) = fileparse($remote_path);
+    my $remote_details = List::Util::first {$_->{filename} eq $remote_name} @{$sftp->ls($remote_dir)};
+    my $epoch = $remote_details->{a}->mtime;
+    my $date = strftime('%Y%m%d', localtime($epoch));
+    my ($suffix) = $remote_name =~ /\.(\w+)$/;
+    my $local_dir = join('/', $plot_directory, 'cnv_aberrant_regions', $donor_name);
+    $local_dir =~ s{//}{/}g;
+    my $local_path = $local_dir . '/' . join('.', $sample, 'cnv_aberrant_regions', $date, $suffix);
+    check_directory_exists($local_dir);
+    $sftp->get($remote_path, $local_path);
   }
   foreach my $cnv_qc (grep {$_->{type} eq 'aberrant_polysomy'} @$donor_qc) {
     print $cnv_polysomy_fh join("\t", @{$cnv_qc}{qw(sample chr)}), "\n";
@@ -92,6 +117,22 @@ foreach my $donor_id (map {$_->{id}} @$donors) {
   foreach my $loh_qc (grep {$_->{type} eq 'loh_calls'} @$donor_qc) {
     print $loh_fh join("\t", @{$loh_qc}{qw(control_sample sample chr start end count)}), "\n";
   }
+
+  foreach my $pluri_plot (grep {$_->{type} eq 'pluritest_plot' && $_->{display_size} eq 'big'} @$donor_qc) {
+    my $remote_path = $pluri_plot->{path};
+    my ($remote_name, $remote_dir) = fileparse($remote_path);
+    my $remote_details = List::Util::first {$_->{filename} eq $remote_name} @{$sftp->ls($remote_dir)};
+    my $epoch = $remote_details->{a}->mtime;
+    my $date = strftime('%Y%m%d', localtime($epoch));
+    my ($suffix) = $remote_name =~ /\.(\w+)$/;
+    my $local_dir = join('/', $plot_directory, 'pluritest', $donor_name);
+    $local_dir =~ s{//}{/}g;
+    my $local_path = $local_dir . '/' . join('.', $donor_name, 'pluritest', $date, $suffix);
+    check_directory_exists($local_dir);
+    $sftp->get($remote_path, $local_path);
+  }
+  
+
 }
 
 close $gender_fh;
