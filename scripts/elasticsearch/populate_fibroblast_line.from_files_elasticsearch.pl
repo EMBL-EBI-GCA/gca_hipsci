3cbc028aca2e29ad7ccaf985334915cb5d9ed48b
peterwharrison <peter@ebi.ac.uk>
2015-10-22 14:29:06 +0100

    Only update cell line for first and not subsequent files, efficency and number logging

--- scripts/elasticsearch/populate_fibroblast_line.from_files_elasticsearch.pl
+++ scripts/elasticsearch/populate_fibroblast_line.from_files_elasticsearch.pl
@@ -44,10 +44,8 @@ foreach my $es_host (@es_host){
   $elasticsearch{$es_host} = ReseqTrack::Tools::HipSci::ElasticsearchClient->new(host => $es_host);
 }
 
+my %nonipsc_celllines;
 while( my( $host, $elasticsearchserver ) = each %elasticsearch ){
-  my $cell_created = 0;
-  my $cell_updated = 0;
-  my $cell_uptodate = 0;
   my $scroll = $elasticsearchserver->call('scroll_helper',
     index       => 'hipsci',
     search_type => 'scan',
@@ -64,87 +62,97 @@ while( my( $host, $elasticsearchserver ) = each %elasticsearch ){
       next SAMPLE if ! $tissue->biosample_id;
       my $biosample = BioSD::fetch_sample($tissue->biosample_id);
       next SAMPLE if !$biosample;
-      my $donor = $tissue->donor;
-      my $donor_biosample = BioSD::fetch_sample($donor->biosample_id);
-      my $tissue_biosample = BioSD::fetch_sample($tissue->biosample_id);
-      my $source_material = $tissue->tissue_type;
-      my $sample_index = {};
+      $nonipsc_celllines{$nonipsc_linename}=1;
+    }
+  }
+}
+while( my( $host, $elasticsearchserver ) = each %elasticsearch ){
+  my $cell_created = 0;
+  my $cell_updated = 0;
+  my $cell_uptodate = 0;
+  foreach my $nonipsc_linename (keys %nonipsc_celllines){
+    my $tissue = $cgap_tissues{$nonipsc_linename};
+    my $biosample = BioSD::fetch_sample($tissue->biosample_id);
+    my $donor = $tissue->donor;
+    my $donor_biosample = BioSD::fetch_sample($donor->biosample_id);
+    my $tissue_biosample = BioSD::fetch_sample($tissue->biosample_id);
+    my $source_material = $tissue->tissue_type;
+    my $sample_index = {};
 
-      $sample_index->{name} = $biosample->property('Sample Name')->values->[0];
-      $sample_index->{'bioSamplesAccession'} = $tissue->biosample_id;
-      $sample_index->{'donor'} = {name => $donor_biosample->property('Sample Name')->values->[0],
-                                bioSamplesAccession => $donor->biosample_id};
+    $sample_index->{name} = $biosample->property('Sample Name')->values->[0];
+    $sample_index->{'bioSamplesAccession'} = $tissue->biosample_id;
+    $sample_index->{'donor'} = {name => $donor_biosample->property('Sample Name')->values->[0],
+                              bioSamplesAccession => $donor->biosample_id};
 
-      $sample_index->{'sourceMaterial'} = {
-        value => $source_material,
-      };
+    $sample_index->{'sourceMaterial'} = {
+      value => $source_material,
+    };
 
-      if (my $cell_type_property = $tissue_biosample->property('cell type')) {
-        my $cell_type_qual_val = $cell_type_property->qualified_values()->[0];
-        my $cell_type_purl = $cell_type_qual_val->term_source()->term_source_id();
-        if ($cell_type_purl !~ /^http:/) {
-          $cell_type_purl = $cell_type_qual_val->term_source()->uri() . $cell_type_purl;
-        }
-        if (ucfirst(lc($cell_type_qual_val->value())) eq "Pbmc"){
-          $sample_index->{'sourceMaterial'}->{cellType} = "PBMC";
-        }else{
-          $sample_index->{'sourceMaterial'}->{cellType} = ucfirst(lc($cell_type_qual_val->value()));
-        }
-        $sample_index->{'sourceMaterial'}->{ontologyPURL} = $cell_type_purl;
-        $sample_index->{'cellType'}->{value} = ucfirst(lc($cell_type_qual_val->value()));
-        $sample_index->{'cellType'}->{ontologyPURL} = $cell_type_purl;
+    if (my $cell_type_property = $tissue_biosample->property('cell type')) {
+      my $cell_type_qual_val = $cell_type_property->qualified_values()->[0];
+      my $cell_type_purl = $cell_type_qual_val->term_source()->term_source_id();
+      if ($cell_type_purl !~ /^http:/) {
+        $cell_type_purl = $cell_type_qual_val->term_source()->uri() . $cell_type_purl;
       }
-
-      if (my $biomaterial_provider = $biomaterial_provider_hash{$donor->hmdmc}) {
-        $sample_index->{'tissueProvider'} = $biomaterial_provider;
-      }
-      $sample_index->{'openAccess'} = $open_access_hash{$donor->hmdmc};
-      my $line_exists = $elasticsearchserver->call('exists',
-        index => 'hipsci',
-        type => 'cellLine',
-        id => $nonipsc_linename,
-      );
-      if ($line_exists){
-        my $original = $elasticsearchserver->fetch_line_by_name($nonipsc_linename);
-        my $update = $elasticsearchserver->fetch_line_by_name($nonipsc_linename);
-        delete $$update{'_source'}{'name'}; 
-        delete $$update{'_source'}{'bioSamplesAccession'}; 
-        delete $$update{'_source'}{'donor'}{'name'}; 
-        delete $$update{'_source'}{'donor'}{'bioSamplesAccession'};
-        if (! scalar keys $$update{'_source'}{'donor'}){
-          delete $$update{'_source'}{'donor'};
-        }
-        delete $$update{'_source'}{'cellType'}{'value'};
-        delete $$update{'_source'}{'cellType'}{'ontologyPURL'};
-        if (! scalar keys $$update{'_source'}{'cellType'}){
-          delete $$update{'_source'}{'cellType'};
-        }
-        delete $$update{'_source'}{'sourceMaterial'}; 
-        delete $$update{'_source'}{'tissueProvider'}; 
-        delete $$update{'_source'}{'openAccess'};
-        foreach my $field (keys %$sample_index){
-          my $subfield = $$sample_index{$field};
-          if (ref($subfield) eq 'HASH'){
-            foreach my $subfield (keys $$sample_index{$field}){
-              $$update{'_source'}{$field}{$subfield} = $$sample_index{$field}{$subfield};
-            }
-          }else{
-            $$update{'_source'}{$field} = $$sample_index{$field};
-          }
-        }
-        if (Compare($$update{'_source'}, $$original{'_source'})){
-          $cell_uptodate++;
-        }else{ 
-          $$update{'_source'}{'_indexUpdated'} = $date;
-          $elasticsearchserver->index_line(id => $sample_index->{name}, body => $$update{'_source'});
-          $cell_updated++;
-        }
+      if (ucfirst(lc($cell_type_qual_val->value())) eq "Pbmc"){
+        $sample_index->{'sourceMaterial'}->{cellType} = "PBMC";
       }else{
-        $sample_index->{'_indexCreated'} = $date;
-        $sample_index->{'_indexUpdated'} = $date;
-        $elasticsearchserver->index_line(id => $sample_index->{name}, body => $sample_index);
-        $cell_created++;
+        $sample_index->{'sourceMaterial'}->{cellType} = ucfirst(lc($cell_type_qual_val->value()));
       }
+      $sample_index->{'sourceMaterial'}->{ontologyPURL} = $cell_type_purl;
+      $sample_index->{'cellType'}->{value} = ucfirst(lc($cell_type_qual_val->value()));
+      $sample_index->{'cellType'}->{ontologyPURL} = $cell_type_purl;
+    }
+
+    if (my $biomaterial_provider = $biomaterial_provider_hash{$donor->hmdmc}) {
+      $sample_index->{'tissueProvider'} = $biomaterial_provider;
+    }
+    $sample_index->{'openAccess'} = $open_access_hash{$donor->hmdmc};
+    my $line_exists = $elasticsearchserver->call('exists',
+      index => 'hipsci',
+      type => 'cellLine',
+      id => $nonipsc_linename,
+    );
+    if ($line_exists){
+      my $original = $elasticsearchserver->fetch_line_by_name($nonipsc_linename);
+      my $update = $elasticsearchserver->fetch_line_by_name($nonipsc_linename);
+      delete $$update{'_source'}{'name'}; 
+      delete $$update{'_source'}{'bioSamplesAccession'}; 
+      delete $$update{'_source'}{'donor'}{'name'}; 
+      delete $$update{'_source'}{'donor'}{'bioSamplesAccession'};
+      if (! scalar keys $$update{'_source'}{'donor'}){
+        delete $$update{'_source'}{'donor'};
+      }
+      delete $$update{'_source'}{'cellType'}{'value'};
+      delete $$update{'_source'}{'cellType'}{'ontologyPURL'};
+      if (! scalar keys $$update{'_source'}{'cellType'}){
+        delete $$update{'_source'}{'cellType'};
+      }
+      delete $$update{'_source'}{'sourceMaterial'}; 
+      delete $$update{'_source'}{'tissueProvider'}; 
+      delete $$update{'_source'}{'openAccess'};
+      foreach my $field (keys %$sample_index){
+        my $subfield = $$sample_index{$field};
+        if (ref($subfield) eq 'HASH'){
+          foreach my $subfield (keys $$sample_index{$field}){
+            $$update{'_source'}{$field}{$subfield} = $$sample_index{$field}{$subfield};
+          }
+        }else{
+          $$update{'_source'}{$field} = $$sample_index{$field};
+        }
+      }
+      if (Compare($$update{'_source'}, $$original{'_source'})){
+        $cell_uptodate++;
+      }else{ 
+        $$update{'_source'}{'_indexUpdated'} = $date;
+        $elasticsearchserver->index_line(id => $sample_index->{name}, body => $$update{'_source'});
+        $cell_updated++;
+      }
+    }else{
+      $sample_index->{'_indexCreated'} = $date;
+      $sample_index->{'_indexUpdated'} = $date;
+      $elasticsearchserver->index_line(id => $sample_index->{name}, body => $sample_index);
+      $cell_created++;
     }
   }
   print "\n$host\n";
