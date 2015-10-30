6f0377708a38659f376d36db57fd7fb75988bbd8
istreeter <streeter@ebi.ac.uk>
2015-10-30 17:25:57 +0000

    Updated the meta info file script for better authorities of growing conditions

--- scripts/cgap_reports/dump_cell_line_info/dump_cell_line_info.qc1.pl
+++ scripts/cgap_reports/dump_cell_line_info/dump_cell_line_info.qc1.pl
@@ -4,11 +4,10 @@ use strict;
 use warnings;
 
 use ReseqTrack::Tools::HipSci::CGaPReport::CGaPReportUtils qw(read_cgap_report);
-use ReseqTrack::Tools::HipSci::CGaPReport::Improved::CGaPReportImprover qw(improve_donors improve_tissues improve_ips_lines);
 use Text::Delimited;
-use DBI;
+use ReseqTrack::Tools::HipSci::ElasticsearchClient;
+use ReseqTrack::DBSQL::DBAdaptor;
 use Getopt::Long;
-use BioSD;
 use List::Util qw();
 use File::Find qw();
 
@@ -16,46 +15,48 @@ use File::Find qw();
   #qw(leeh_3 iakz_1 febc_2 nibo_3 aehn_2 oarz_22 zisa_33 peop_4 dard_2 coxy_33 xisg_33 oomz_22 dovq_33 liun_22 xavk_33 aehn_22 funy_1 funy_3 giuf_1 giuf_3 iill_1 iill_3 bima_1 bima_2 ieki_2 ieki_3 qolg_1 qolg_3 bulb_1 gusc_1 gusc_2 gusc_3)
 #);
 
-my $demographic_filename;
-my $growing_conditions_filename;
-my $cnv_filename;
-my $pluritest_filename;
-my $gtarray_allowed_samples_filename;
-my $gexarray_allowed_samples_filename;
+
 my $ag_lims_filename;
 my $sendai_counts_dir;
+my $pluritest_filename;
+my $es_host='ves-pg-e4:9200';
+my $dbhost = 'mysql-g1kdcc-public';
+my $dbuser = 'g1kro';
+my $dbpass;
+my $dbport = 4197;
+my $dbname = 'hipsci_private_track';
+my $gexarray_allowed_samples_filename;
 &GetOptions(
-  'demographic_file=s' => \$demographic_filename,
-  'growing_conditions_file=s' => \$growing_conditions_filename,
+  'es_host=s' => \$es_host,
   'pluritest_file=s' => \$pluritest_filename,
-  'cnv_filename=s' => \$cnv_filename,
-  'gtarray_allowed_samples_filename=s' => \$gtarray_allowed_samples_filename,
-  'gexarray_allowed_samples_filename=s' => \$gexarray_allowed_samples_filename,
   'ag_lims_fields=s' => \$ag_lims_filename,
   'sendai_counts_dir=s' => \$sendai_counts_dir,
+  'pluritest_file=s' => \$pluritest_filename,
+    'dbhost=s'      => \$dbhost,
+    'dbname=s'      => \$dbname,
+    'dbuser=s'      => \$dbuser,
+    'dbpass=s'      => \$dbpass,
+    'dbport=s'      => \$dbport,
+    'gexarray_allowed_samples_filename=s' => \$gexarray_allowed_samples_filename,
 );
 
-die "did not get a demographic file on the command line" if !$demographic_filename;
 die "did not get a sendai_counts_dir on the command line" if !$sendai_counts_dir;
 
-my ($donors, $tissues, $ips_lines) = @{read_cgap_report(days_old=>3)}{qw(donors tissues ips_lines)};
-$donors = improve_donors(donors=>$donors, demographic_file=>$demographic_filename);
-$tissues = improve_tissues(tissues=>$tissues);
-$ips_lines = improve_ips_lines(ips_lines=>$ips_lines, growing_conditions_file =>$growing_conditions_filename);
+my $elasticsearch = ReseqTrack::Tools::HipSci::ElasticsearchClient->new(host => $es_host);
 
-my %gtarray_allowed_samples;
-open my $qc1_fh, '<', $gtarray_allowed_samples_filename or die "could not open $gtarray_allowed_samples_filename $!";
-LINE:
-while (my $line = <$qc1_fh>) {
-  chomp $line;
-  my @split_line =split("\t", $line);
-  next LINE if !$split_line[0] || !$split_line[1];
-  $gtarray_allowed_samples{join('_', @split_line[0,1])} = $split_line[0];
-}
-close $qc1_fh;
+my $db = ReseqTrack::DBSQL::DBAdaptor->new(
+  -host => $dbhost,
+  -user => $dbuser,
+  -port => $dbport,
+  -dbname => $dbname,
+  -pass => $dbpass,
+    );
+my $fa = $db->get_FileAdaptor;
+
+my ($donors, $tissues, $ips_lines) = @{read_cgap_report(days_old=>3)}{qw(donors tissues ips_lines)};
 
 my %gexarray_allowed_samples;
-open $qc1_fh, '<', $gexarray_allowed_samples_filename or die "could not open $gexarray_allowed_samples_filename $!";
+open my $qc1_fh, '<', $gexarray_allowed_samples_filename or die "could not open $gexarray_allowed_samples_filename $!";
 LINE:
 while (my $line = <$qc1_fh>) {
   chomp $line;
@@ -64,19 +65,6 @@ while (my $line = <$qc1_fh>) {
   $gexarray_allowed_samples{join('_', @split_line[0,1])} = $split_line[0];
 }
 close $qc1_fh;
-
-my %cnv_details;
-open my $cnv_fh, '<', $cnv_filename or die "could not open $cnv_filename $!";
-<$cnv_fh>;
-LINE:
-while (my $line = <$cnv_fh>) {
-  chomp $line;
-  my @split_line = split("\t", $line);
-  my $allowed_cell_line = $gtarray_allowed_samples{$split_line[0]};
-  next LINE if !$allowed_cell_line;
-  $cnv_details{$allowed_cell_line} = \@split_line;
-}
-close $cnv_fh;
 
 my %pluritest_details;
 open my $pluri_fh, '<', $pluritest_filename or die "could not open $pluritest_filename $!";
@@ -94,9 +82,9 @@ close $pluri_fh;
 my $ag_lims_file = new Text::Delimited;
 $ag_lims_file->delimiter(';');
 my %ag_lims_fields;
-$ag_lims_file->open($ag_lims_filename) or die "could not open $demographic_filename $!";
+$ag_lims_file->open($ag_lims_filename) or die "could not open $ag_lims_filename $!";
 while (my $line_data = $ag_lims_file->read) {
-  $ag_lims_fields{$line_data->{'id_public.name'}} = $line_data;
+  $ag_lims_fields{$line_data->{'name'}} = $line_data;
 }
 $ag_lims_file->close;
 
@@ -131,7 +119,7 @@ my @ag_lims_output_fields = qw(id_lims id_qc1 id_qc2 time_registration time_puri
     assaybatch_methyl.sentrix.id assaybatch_methyl.sentrix.position assaybatch_methyl.plate
     assaybatch_methyl.well checks_pluritest.raw checks_pluritest.novelty checks_gex.fail
     checks_passage.rate checks_qc2.swap checks_cnvs study_blueprint
-    study_reprogramming.comparison study_media.comparison comment_qc1.decision comment_anja);
+    study_reprogramming.comparison study_media.comparison comment_qc1.decision comment_anja friendly);
 print join("\t", @output_fields, @ag_lims_output_fields), "\n";
 
 my @output_lines;
@@ -139,28 +127,24 @@ DONOR:
 foreach my $donor (@$donors) {
   my $donor_name = '';
   if (my $donor_biosample_id = $donor->biosample_id) {
-    if (my $donor_biosample = BioSD::fetch_sample($donor_biosample_id)) {
-      $donor_name = $donor_biosample->property('Sample Name')->values->[0];
+    if (my $es_donor = $elasticsearch->fetch_donor_by_biosample_id($donor_biosample_id)) {
+      $donor_name = $es_donor->{_source}{name};
     }
   }
   TISSUE:
   foreach my $tissue (@{$donor->tissues}) {
-    my $tissue_has_data = 0;
 
+    my $es_line;
     IPS_LINE:
     foreach my $ips_line (@{$tissue->ips_lines}) {
       next IPS_LINE if !$ips_line->biosample_id;
-      #next IPS_LINE if !$allowed_samples{$ips_line->name};
       next IPS_LINE if $ips_line->name !~ /HPSI/;
-      my $reprogramming_tech = $ips_line->reprogramming_tech;
-      $reprogramming_tech = $reprogramming_tech ? lc($reprogramming_tech) : undef;
-      if ($reprogramming_tech =~ /cytotune/) {
-        $reprogramming_tech = 'sendai';
+      if (my $next_es_line = $elasticsearch->fetch_line_by_name($ips_line->name)) {
+        $es_line = $next_es_line;
       }
-
-      #if (scalar grep { $ips_line->name =~ m/$_$/ } @feeder_free_temp_override) {
-        #$growing_conditions = 'E8';
-      #}
+      else {
+        next IPS_LINE;
+      }
 
       my %output = (name => $ips_line->name,
           cell_type => 'iPSC',
@@ -169,21 +153,15 @@ foreach my $donor (@$donors) {
           biosample_id => $ips_line->biosample_id,
           tissue_biosample_id => $tissue->biosample_id,
           donor_biosample_id => $donor->biosample_id,
-          derived_from_cell_type => $tissue->type,
-          reprogramming => $reprogramming_tech,
-          gender => $donor->gender,
-          age => $donor->age,
-          disease => $donor->disease,
-          ethnicity => $donor->ethnicity,
-          growing_conditions_gtarray => $ips_line->growing_conditions_qc1,
-          growing_conditions_gexarray => $ips_line->growing_conditions_qc1,
-          growing_conditions_mtarray => $ips_line->growing_conditions_qc2,
-          growing_conditions_rnaseq => $ips_line->growing_conditions_qc2,
-          growing_conditions_exomeseq => $ips_line->growing_conditions_qc2,
-          growing_conditions_proteomics => $ips_line->growing_conditions_qc2,
-          cnv_num_different_regions => $cnv_details{$ips_line->name}->[1],
-          cnv_length_different_regions_Mbp => $cnv_details{$ips_line->name}->[2],
-          cnv_length_shared_differences_Mbp => $cnv_details{$ips_line->name}->[3],
+          derived_from_cell_type => $es_line->{_source}{sourceMaterial}{cellType},
+          reprogramming => $es_line->{_source}{reprogramming}{methodOfDerivation},
+          gender => lc($es_line->{_source}{donor}{sex}{value} || ''),
+          age => $es_line->{_source}{donor}{age},
+          disease => $es_line->{_source}{diseaseStatus}{value},
+          ethnicity => $es_line->{_source}{donor}{ethnicity},
+          cnv_num_different_regions => $es_line->{_source}{cnv}{num_different_regions},
+          cnv_length_different_regions_Mbp => $es_line->{_source}{cnv}{length_different_regions_Mbp},
+          cnv_length_shared_differences_Mbp => $es_line->{_source}{cnv}{length_shared_differences},
           pluri_raw => $pluritest_details{$ips_line->name}->[1],
           pluri_logit_p => $pluritest_details{$ips_line->name}->[2],
           pluri_novelty => $pluritest_details{$ips_line->name}->[3],
@@ -191,6 +169,31 @@ foreach my $donor (@$donors) {
           pluri_rmsd => $pluritest_details{$ips_line->name}->[5],
           'rnaseq.sendai_reads' => $rna_sendai_reads{$ips_line->name},
       );
+      ASSAY:
+      foreach my $assay(qw(gtarray gexarray mtarray rnaseq exomeseq proteomics)) {
+        my $release_type = $assay =~ /g.*array/ ? 'qc1' : 'qc2';
+        my ($file) = grep {$_->name !~ m{/withdrawn/}} @{$fa->fetch_by_filename('%/'.$assay.'/%/'.$ips_line->name.'/%')};
+        next ASSAY if !$file;
+
+        my $date;
+        if ($assay eq 'proteomics') {
+          $date = $file->created;
+        }
+        else {
+          ($date) = $file->name =~ /\.(\d{8})\./;
+        }
+        next ASSAY if !$date;
+
+        my $cgap_release = $ips_line->get_release_for(type => $release_type, date =>$date);
+        my $growing_conditions = $cgap_release && $cgap_release->is_feeder_free ? 'Feeder-free'
+                        : $cgap_release && !$cgap_release->is_feeder_free ? 'Feeder-dependent'
+                        : $ips_line->name =~ /_\d\d$/ ? 'Feeder-free'
+                        : $ips_line->passage_ips && $ips_line->passage_ips lt 20140000 ? 'Feeder-dependent'
+                        : $ips_line->qc1 && $ips_line->qc1 lt 20140000 ? 'Feeder-dependent'
+                        : die "could not get growing conditions for ".$file->name;
+        $output{'growing_conditions_'.$assay} = $growing_conditions;
+      }
+
       if (my $ips_ag_lims_fields = $ag_lims_fields{$ips_line->name}) {
         foreach my $output_field (@ag_lims_output_fields) {
           if (my $field_val = $ips_ag_lims_fields->{$output_field}) {
@@ -200,18 +203,17 @@ foreach my $donor (@$donors) {
       };
       my (@sort_parts) = $ips_line->name =~ /\w+(\d\d)(\d\d)\w*-([a-z]+)_(\d+)/;
       push(@output_lines, [\@sort_parts, join("\t", map {$_ // ''} @output{@output_fields, @ag_lims_output_fields})]);
-      $tissue_has_data = 1;
 
     }
-    next TISSUE if !$tissue_has_data;
+    next TISSUE if !$es_line;
     my %output = (name => $tissue->name,
-        cell_type => $tissue->type,
+        cell_type => $es_line->{_source}{sourceMaterial}{cellType},
         biosample_id => $tissue->biosample_id,
         donor_biosample_id => $donor->biosample_id,
-        gender => $donor->gender,
-        age => $donor->age,
-        disease => $donor->disease,
-        ethnicity => $donor->ethnicity,
+        gender => lc($es_line->{_source}{donor}{sex}{value} || ''),
+        age => $es_line->{_source}{donor}{age},
+        disease => $es_line->{_source}{diseaseStatus}{value},
+        ethnicity => $es_line->{_source}{donor}{ethnicity},
         pluri_raw => $pluritest_details{$tissue->name}->[1],
         pluri_logit_p => $pluritest_details{$tissue->name}->[2],
         pluri_novelty => $pluritest_details{$tissue->name}->[3],
