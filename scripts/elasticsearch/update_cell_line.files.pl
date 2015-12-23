5e0fdfe5a2af2334bc5a5dc601cbf88cb0fb3579
istreeter <streeter@ebi.ac.uk>
2015-12-23 15:55:20 +0000

    Update the loading script for the new cell line format

--- scripts/elasticsearch/update_cell_line.files.pl
+++ scripts/elasticsearch/update_cell_line.files.pl
@@ -9,7 +9,6 @@ use List::Util qw();
 use Data::Compare;
 use Getopt::Long;
 use POSIX qw(strftime);
-use Clone qw(clone);
 
 my $date = strftime('%Y%m%d', localtime);
 
@@ -31,16 +30,6 @@ my $scroll = $elasticsearch{$es_host[0]}->call('scroll_helper',
   size        => 500
 );
 
-my %assay_name_map = (
-  'Proteomics' => 'proteomics',
-  'Genotyping array' => 'gtarray',
-  'RNA-seq' => 'rnaseq',
-  'Cellular phenotyping' => 'cellbiol-fn',
-  'Methylation array' => 'mtarray',
-  'Expression array' => 'gexarray',
-  'Exome-seq' => 'exomeseq',
-  'ChIP-seq' => 'chipseq',
-);
 my %ontology_map = (
   'Proteomics' => 'http://www.ebi.ac.uk/efo/EFO_0002766',
   'Genotyping array' => 'http://www.ebi.ac.uk/efo/EFO_0002767',
@@ -50,36 +39,17 @@ my %ontology_map = (
   'Expression array' => 'http://www.ebi.ac.uk/efo/EFO_0002770',
   'Exome-seq' => 'http://www.ebi.ac.uk/efo/EFO_0005396',
   'ChIP-seq' => 'http://www.ebi.ac.uk/efo/EFO_0002692',
+  'Whole genome sequencing' => 'http://www.ebi.ac.uk/efo/EFO_0003744',
 );
-my %archive_map = (
-  'HipSci FTP' => 'FTP',
-  'EGA' => 'EGA',
-  'ENA' => 'ENA',
-  'ArrayExpress' => 'ArrayExpress',
-);
-my %cell_line_updates;
+my %cell_line_assays;
 while ( my $doc = $scroll->next ) {
+  my $assay = $doc->{_source}{assay}{type};
   SAMPLE:
   foreach my $sample (@{$$doc{'_source'}{'samples'}}){
-    my $sample = $$sample{name};
-    my $assay = $$doc{'_source'}{assay}{type};
-    my @urlparts = split('/', $$doc{'_source'}{archive}{url});
-    $cell_line_updates{$sample}{assays}{$assay_name_map{$assay}} = {
-        'archive' => $archive_map{$$doc{'_source'}{archive}{name}},
-        'name' => $assay,
-        'ontologyPURL' => $ontology_map{$assay},
-    };
-    if ($$doc{'_source'}{archive}{name} ne 'EGA' and $$doc{'_source'}{archive}{name} ne 'ENA'){
-      my $ftpurl = $$doc{'_source'}{archive}{url};
-      $ftpurl =~ s?ftp\:\/\/ftp\.hipsci\.ebi\.ac\.uk??;
-      $ftpurl =~ s?raw_open_data?raw_data?;
-      $cell_line_updates{$sample}{assays}{$assay_name_map{$assay}}{path} = $ftpurl;
-    }
-    #NOTE Condsider including study ID. Would need to look it up in EGA, ENA using dataset ID from files table.
+    $cell_line_assays{$sample->{name}}{$assay} = $ontology_map{$assay};
   }
 }
 
-#NOTE Non file based linking such as peptracker will need to be added seperately here
 
 while( my( $host, $elasticsearchserver ) = each %elasticsearch ){
   my $cell_updated = 0;
@@ -93,25 +63,16 @@ while( my( $host, $elasticsearchserver ) = each %elasticsearch ){
 
   CELL_LINE:
   while ( my $doc = $scroll->next ) {
-    my $update = clone $doc;
-    delete $$update{'_source'}{'assays'};
-    if ($cell_line_updates{$$doc{'_source'}{'name'}}){
-      my $lineupdate = $cell_line_updates{$$doc{'_source'}{'name'}};
-      foreach my $field (keys $lineupdate){
-        foreach my $subfield (keys $$lineupdate{$field}){
-          $$update{'_source'}{$field}{$subfield} = $$lineupdate{$field}{$subfield};
-        }
-      }
+    my $cell_line  = $doc->{_source}{name};
+    my @new_assays = map {{name => $_, ontologyPURL => $cell_line_assays{$cell_line}{$_}}} keys %{$cell_line_assays{$cell_line}};
+    next CELL_LINE if Compare(\@new_assays, $doc->{_source}{assays} || []);
+    if (scalar @new_assays) {
+      $doc->{_source}{assays} = \@new_assays;
     }
-    if (Compare($$update{'_source'}, $$doc{'_source'})){
-      $cell_uptodate++;
-    }else{
-      $$update{'_source'}{'_indexUpdated'} = $date;
-      $elasticsearchserver->index_line(id => $$doc{'_source'}{'name'}, body => $$update{'_source'});
-      $cell_updated++;
+    else {
+      delete $doc->{_source}{assays};
     }
+    $doc->{_source}{_indexUpdated} = $date;
+    $elasticsearchserver->index_line(id => $doc->{_source}{name}, body => $doc->{_source});
   }
-  print "\n$host\n";
-  print "07_Availible_Files_update\n";
-  print "Cell lines: $cell_updated updated, $cell_uptodate unchanged.\n";
 }
