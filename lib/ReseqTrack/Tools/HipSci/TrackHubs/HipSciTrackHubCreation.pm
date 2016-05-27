6d3e54631cf70deb7a7a4784a59d60a2201dd3f6
peterwharrison <peter@ebi.ac.uk>
2016-05-27 11:46:54 +0100

    Create basic hub.txt

--- lib/ReseqTrack/Tools/HipSci/TrackHubs/HipSciTrackHubCreation.pm
+++ lib/ReseqTrack/Tools/HipSci/TrackHubs/HipSciTrackHubCreation.pm
@@ -30,16 +30,10 @@ sub make_track_hub{ # main method, creates the track hub of a study in the folde
   my $data = shift;
 
   $self->make_study_dir($server_dir_full_path, $study_id);
-}
+  #TODO decide if needed
+  #$self->make_assemblies_dirs($server_dir_full_path, $study_id, $data);
 
-sub make_study_dir{
-
-  my $self= shift;
-  my $server_dir_full_path= shift;
-  my $study_id = shift;
-
-  run_system_command("mkdir $server_dir_full_path" . '/' . $study_id)
-    or die "I cannot make dir $server_dir_full_path/$study_id in script: ".__FILE__." line: ".__LINE__."\n";
+  $self->make_hubtxt_file($server_dir_full_path , $study_id);
 }
 
 sub run_system_command {
@@ -54,6 +48,54 @@ sub run_system_command {
   }else{
      return 1;
   }
+}
+
+sub make_study_dir{
+
+  my $self= shift;
+  my $server_dir_full_path= shift;
+  my $study_id = shift;
+
+  run_system_command("mkdir $server_dir_full_path" . '/' . $study_id)
+    or die "I cannot make dir $server_dir_full_path/$study_id in script: ".__FILE__." line: ".__LINE__."\n";
+}
+
+# sub make_assemblies_dirs{
+
+#   my $self= shift;
+#   my $server_dir_full_path= shift;
+#   my $study_id = shift;
+#   my $data = shift;
+  
+#   #FIXME
+#   # For every assembly I make a directory for the study -track hub
+#   foreach my $assembly_name (keys %{$study_obj->get_assembly_names}){
+
+#     run_system_command("mkdir $server_dir_full_path/$study_id/$assembly_name")
+#       or die "I cannot make directories of assemblies in $server_dir_full_path/$study_id in script: ".__FILE__." line: ".__LINE__."\n";
+#   }
+# }
+
+sub make_hubtxt_file{
+
+  my $self= shift;
+  my $server_dir_full_path= shift;
+  my $study_id = shift;
+  my $hub_txt_file= "$server_dir_full_path/$study_id/hub.txt";
+
+  run_system_command("touch $hub_txt_file")
+    or die "Could not create hub.txt file in the $server_dir_full_path location\n";
+  
+  open(my $fh, '>', $hub_txt_file) or die "Could not open file '$hub_txt_file' $! in ".__FILE__." line: ".__LINE__."\n";
+
+  print $fh "hub $study_id\n";
+
+  print $fh "shortLabel "."HipSci TrackHub for ".$study_id."\n"; 
+  #TODO Check what to display on long label
+  print $fh "longLabel "."Human Induced Pluripotent Stem Cells Initiative (HipSci) TrackHub for HipSci cell line ".$study_id."\n"; 
+  print $fh "genomesFile genomes.txt\n";
+  print $fh "email hipsci-dcc\@ebi.ac.uk\n";
+  close($fh);
 }
 
 1;
