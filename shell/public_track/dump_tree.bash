e5541a8251c0fb48517f0d6c2781fef63b02d664
Peter Harrison <peter@ebi.ac.uk>
2016-08-08 16:11:23 +0100

    Set log directory for tree dumping

--- shell/public_track/dump_tree.bash
+++ shell/public_track/dump_tree.bash
@@ -5,4 +5,4 @@ perl $RESEQTRACK/scripts/file/run_tree_for_ftp.pl \
   -skip_load -skip_archive \
   -dir_to_tree /nfs/hipsci/vol1/ftp \
   -staging_dir /nfs/1000g-work/hipsci/archive_staging/ftp \
-  -log_dir $PWD/logging_dir
+  -log_dir /homes/hipdcc/logs/hipsci_ftp_tree_logs
