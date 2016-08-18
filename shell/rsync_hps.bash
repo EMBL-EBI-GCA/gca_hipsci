43956d67515764e2410f3f438d374d9e90cbf909
istreeter <streeter@ebi.ac.uk>
2016-08-18 16:17:39 +0100

    Better rsyncing, accouts for deleted vcf files

--- shell/rsync_hps.bash
+++ shell/rsync_hps.bash
@@ -2,12 +2,10 @@
 
 FROM=/nfs/research2/hipsci/drop/hip-drop/tracked
 TO=/hps/nobackup/research/hipsci/tracked
-REGEX='.*\.vcf(\.gz)?(\.tbi)?'
 
-find $FROM -regextype posix-awk -regex $REGEX -printf %P\\0 | rsync -ptgo --files-from=- --from0 ${FROM}/ $TO
+rsync -ptgomr --delete-during --include='*/' --include='*.vcf' --include='*.vcf.gz' --include='*.vcf.gz' --exclude='*' ${FROM}/ $TO
 
 FROM=/nfs/research2/hipsci/controlled
 TO=/hps/nobackup/research/hipsci/controlled
-REGEX='.*\.vcf(\.gz)?(\.tbi)?'
 
-find $FROM -regextype posix-awk -regex $REGEX -printf %P\\0 | rsync -ptgo --files-from=- --from0 ${FROM}/ $TO
+rsync -ptgomr --delete-during --include='*/' --include='*.vcf' --include='*.vcf.gz' --include='*.vcf.gz' --exclude='*' ${FROM}/ $TO
