9b4df18614c68c231d254e4c7179884771289368
istreeter <streeter@ebi.ac.uk>
2016-08-18 14:37:14 +0100

    modifications to the rsync script

--- shell/rsync_hps.bash
+++ shell/rsync_hps.bash
@@ -4,10 +4,10 @@ FROM=/nfs/research2/hipsci/drop/hip-drop/tracked
 TO=/hps/nobackup/research/hipsci/tracked
 REGEX='.*\.vcf(\.gz)?(\.tbi)?'
 
-find $FROM -regextype posix-awk -type f -printf %P\\0 | rsync -ptgo --files-from=- ${FROM}/ $TO
+find $FROM -regextype posix-awk -regex $REGEX -printf %P\\0 | rsync -ptgo --files-from=- --from0 ${FROM}/ $TO
 
 FROM=/nfs/research2/hipsci/controlled
 TO=/hps/nobackup/research/hipsci/controlled
 REGEX='.*\.vcf(\.gz)?(\.tbi)?'
 
-find $FROM -regextype posix-awk -regex $REGEX -printf %P\\0 | rsync -ptgo --files-from=- ${FROM}/ $TO
+find $FROM -regextype posix-awk -regex $REGEX -printf %P\\0 | rsync -ptgo --files-from=- --from0 ${FROM}/ $TO
