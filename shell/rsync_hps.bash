6da6fa52f9098f912891e024b0cf111e125e8fcd
istreeter <streeter@ebi.ac.uk>
2016-08-22 11:53:52 +0100

    rsync to yoda, instead of to HH

--- shell/rsync_hps.bash
+++ shell/rsync_hps.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 FROM=/nfs/research2/hipsci/drop/hip-drop/tracked
-TO=/hps/nobackup/research/hipsci/tracked
+TO=yoda-login:/hps/nobackup/hipsci/tracked
 
-rsync -ptgomr --delete-during --include='*/' --include='*.vcf' --include='*.vcf.gz' --include='*.vcf.gz' --exclude='*' ${FROM}/ $TO
+rsync -ptgomr -e "ssh -o StrictHostKeyChecking=no" --delete-during --include='*/' --include='*.vcf' --include='*.vcf.gz' --include='*.vcf.gz' --exclude='*' ${FROM}/ $TO
 
 FROM=/nfs/research2/hipsci/controlled
-TO=/hps/nobackup/research/hipsci/controlled
+TO=yoda-login:/hps/nobackup/hipsci/controlled
 
-rsync -ptgomr --delete-during --include='*/' --include='*.vcf' --include='*.vcf.gz' --include='*.vcf.gz' --exclude='*' ${FROM}/ $TO
+rsync -ptgomr -e "ssh -o StrictHostKeyChecking=no" --delete-during --include='*/' --include='*.vcf' --include='*.vcf.gz' --include='*.vcf.gz' --exclude='*' ${FROM}/ $TO
