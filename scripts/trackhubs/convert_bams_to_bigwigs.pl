d217fb87c5114b7b0ecc2239259b166e33656a89
Peter Harrison <peter@ebi.ac.uk>
2017-02-03 13:38:09 +0000

    Convert to cpgBigWig module and perl to c code

--- scripts/trackhubs/convert_bams_to_bigwigs.pl
+++ scripts/trackhubs/convert_bams_to_bigwigs.pl
@@ -3,14 +3,13 @@
 use strict;
 use warnings;
 
-#Requires threaded perl environment "perlbrew use perl-5.16.3_RH7_threaded;source ~/.pcap_bashrc"
+#Requires .cgpBigWig_bashrc
 
 use Getopt::Long;
 use File::Find;
 use ReseqTrack::Tools::BatchSubmission::LSF;
 
-my ($bamlocaldir, $wigoutputdir, $reference, $flags, $bamToBwpath, $farmlogfolder, $threadedperlbrew);
-my $threads = 4;
+my ($bamlocaldir, $wigoutputdir, $reference, $flags, $bamToBwpath, $farmlogfolder);
 
 GetOptions(
   "bamlocaldir=s" => \$bamlocaldir,
@@ -37,15 +36,16 @@ my $lsf = ReseqTrack::Tools::BatchSubmission::LSF->new(
   -max_job_number => 10000,
   -sleep => 360,
 );
-#system "perlbrew use $threadedperlbrew";
+
 foreach my $file (@bamfiles){
   if ($file =~ /.bam$/){
     my $sample = "bam2bw-".(split(/\./, (split(/\//, $file))[-1]))[0];
     my $farmlogfile = $farmlogfolder."/".$sample.".log";
     my $wigoutputdir = $wigoutputdir."/".$sample."/";
-    my $cmd_to_run = "perl $bamToBwpath -b $file -o $wigoutputdir -r $reference -threads $threads -f $flags";
-    my $bsub_cmd = $lsf->construct_command_line($cmd_to_run, "-q production-rh7", $farmlogfile, $sample, 0);
-    my $result = `$bsub_cmd 2>&1`;
-    print $result, "\n";
+    my $cmd_to_run = "$bamToBwpath -i $file -o $wigoutputdir -F $flags -r $reference";
+    #my $bsub_cmd = $lsf->construct_command_line($cmd_to_run, "-q production-rh7", $farmlogfile, $sample, 0);
+    #my $result = `$bsub_cmd 2>&1`;
+    #print $result, "\n";
+    print $cmd_to_run, "\n";
   }
 }
