492301673c214225ddf7391dda1f9966ae95fbeb
istreeter <streeter@ebi.ac.uk>
2017-04-27 15:44:21 +0100

    Deleting some unused files

--- scripts/indexes/temp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-perl $HIPSCI_CODE/scripts/indexes/ena_index.analyses.es.pl \
-  -era_password $RESEQTRACK_PASS \
-  -study_id ERP013161 \
-  -study_id ERP013162 \
-  -demographic /nfs/production/reseq-info/work/streeter/hipsci/resources/Demographicdata_HipSci_2015-05-19.csv
