99bc00b5d8b37e723e6c5a80a390e59e343c7e26
Peter Harrison <peter@ebi.ac.uk>
2017-09-25 12:36:00 +0100

    Add example command to dump qc1 info

--- scripts/cgap_reports/dump_cell_line_info/dump_cell_line_info.qc1.pl
+++ scripts/cgap_reports/dump_cell_line_info/dump_cell_line_info.qc1.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/env perl
 
+###############################################
+#Example command line:
+#perl dump_cell_line_info.qc1.pl -sendai_counts_dir /nfs/research2/hipsci/drop/hip-drop/tracked/rnaseq/sendai_alignment/ -pluritest_file /nfs/research2/hipsci/drop/hip-drop/tracked/qc1_raw_data/hipsci.qc1.20170503.pluritest.tsv -ag_lims_fields /nfs/research2/hipsci/tracking_resources/AG_metadata/AG.metadata.20161018.tsv > /nfs/research2/hipsci/drop/hip-drop/tracked/sample_meta_data/hipsci.qc1_sample_info.20170925.tsv
+###############################################
+
 use strict;
 use warnings;
 
@@ -10,11 +15,11 @@ use ReseqTrack::DBSQL::DBAdaptor;
 use Getopt::Long;
 use List::Util qw();
 use File::Find qw();
+use Data::Dumper;
 
 #my @feeder_free_temp_override = (
   #qw(leeh_3 iakz_1 febc_2 nibo_3 aehn_2 oarz_22 zisa_33 peop_4 dard_2 coxy_33 xisg_33 oomz_22 dovq_33 liun_22 xavk_33 aehn_22 funy_1 funy_3 giuf_1 giuf_3 iill_1 iill_3 bima_1 bima_2 ieki_2 ieki_3 qolg_1 qolg_3 bulb_1 gusc_1 gusc_2 gusc_3)
 #);
-
 
 my $ag_lims_filename;
 my $sendai_counts_dir;
@@ -27,10 +32,9 @@ my $dbport = 4197;
 my $dbname = 'hipsci_private_track';
 &GetOptions(
   'es_host=s' => \$es_host,
-  'pluritest_file=s' => \$pluritest_filename,
-  'ag_lims_fields=s' => \$ag_lims_filename,
-  'sendai_counts_dir=s' => \$sendai_counts_dir,
-  'pluritest_file=s' => \$pluritest_filename,
+  'pluritest_file=s' => \$pluritest_filename, #/nfs/research2/hipsci/drop/hip-drop/tracked/qc1_raw_data/hipsci.qc1.20170503.pluritest.tsv
+  'ag_lims_fields=s' => \$ag_lims_filename, #cgap_dnap_reports
+  'sendai_counts_dir=s' => \$sendai_counts_dir, #/tracked/rnaseq/sendai_alignment
     'dbhost=s'      => \$dbhost,
     'dbname=s'      => \$dbname,
     'dbuser=s'      => \$dbuser,
@@ -76,8 +80,6 @@ foreach my $line (keys %pluritest_details) {
     delete $pluritest_details{$line};
   }
 }
-
-
 
 my %ag_lims_fields;
 if ($ag_lims_filename) {
