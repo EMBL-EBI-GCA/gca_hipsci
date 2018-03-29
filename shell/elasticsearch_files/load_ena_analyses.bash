72c18a0c9fd9fe85d0e73c3946ca59050f77495c
Peter Harrison <peter@ebi.ac.uk>
2018-03-29 14:28:36 +0100

    Add ENA study to HipSci data

--- shell/elasticsearch_files/load_ena_analyses.bash
+++ shell/elasticsearch_files/load_ena_analyses.bash
@@ -16,6 +16,7 @@ perl $HIPSCI_CODE/scripts/indexes/ena_index.analyses.es.pl \
  -study_id ERP013162 \
  -study_id ERP013436 \
  -study_id ERP017015 \
+ -study_id ERP016335 \
  -analysis_study_id ERP013157=ERP006946 \
  -analysis_study_id ERP013158=ERP006946 \
  -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
