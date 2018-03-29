84a5c054dfda8794b1de2b2cb94e735d59043cc9
Peter Harrison <peter@ebi.ac.uk>
2018-03-29 14:04:26 +0100

    Update demographic data URL

--- shell/elasticsearch_files/load_cellbiol.bash
+++ shell/elasticsearch_files/load_cellbiol.bash
@@ -5,4 +5,4 @@ HIPSCI_CODE=`dirname $0`/../..
 # Run this one whenver new cellular phenotyping images get put on the public ftp site
 
 perl $HIPSCI_CODE/scripts/indexes/cellbiol-fn.es.pl \
-  -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-12.csv
+  -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
