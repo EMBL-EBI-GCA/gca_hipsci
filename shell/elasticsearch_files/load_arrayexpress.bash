08876ce4fe127221e202408fab95fab9f0e53b8f
Peter Harrison <peter@ebi.ac.uk>
2018-03-29 13:46:54 +0100

    Add arrayexpress project E-MTAB-4748

--- shell/elasticsearch_files/load_arrayexpress.bash
+++ shell/elasticsearch_files/load_arrayexpress.bash
@@ -8,4 +8,5 @@ HIPSCI_CODE=`dirname $0`/../..
 perl $HIPSCI_CODE/scripts/indexes/arrayexpress_dataset_index.arrays.es.pl \
   -dataset_id E-MTAB-4057 \
   -dataset_id E-MTAB-4059 \
+  -dataset_id E-MTAB-4748 \
   -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
