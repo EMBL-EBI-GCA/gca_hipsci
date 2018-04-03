721731229860e79ad6005a0ac180a7331cdc2339
Peter Harrison <peter@ebi.ac.uk>
2018-04-03 09:16:12 +0100

    Update EGA exomeseq and rnaseq ids

--- shell/elasticsearch_files/load_ega_analyses.bash
+++ shell/elasticsearch_files/load_ega_analyses.bash
@@ -7,9 +7,25 @@ HIPSCI_CODE=`dirname $0`/../..
 
 perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.analyses.es.pl \
   -era_password $RESEQTRACK_PASS \
+  -dataset_id EGAD00001000893 \
+  -dataset_id EGAD00001000897 \
+  -dataset_id EGAD00001001422 \
+  -dataset_id EGAD00001001437 \
+  -dataset_id EGAD00001001438 \
+  -dataset_id EGAD00001001932 \
+  -dataset_id EGAD00001001933 \
+  -dataset_id EGAD00001001949 \
+  -dataset_id EGAD00001001950 \
+  -dataset_id EGAD00001001951 \
+  -dataset_id EGAD00001001952 \
+  -dataset_id EGAD00001001953 \
+  -dataset_id EGAD00001001954 \
+  -dataset_id EGAD00001001955 \
+  -dataset_id EGAD00001001999 \
+  -dataset_id EGAD00001002000 \
   -dataset_id EGAD00001003161 \
-  -dataset_id EGAD00001003514 \
-  -dataset_id EGAD00001003515 \
+  -dataset_id EGAD00001003180 \
+  -dataset_id EGAD00001003181 \
   -dataset_id EGAD00001003516 \
   -dataset_id EGAD00001003517 \
   -dataset_id EGAD00001003518 \
@@ -27,7 +43,6 @@ perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.analyses.es.pl \
   -dataset_id EGAD00001003530 \
   -dataset_id EGAD00001003531 \
   -dataset_id EGAD00001003532 \
-  -dataset_id EGAD00001003533 \
   -dataset_id EGAD00001003534 \
   -dataset_id EGAD00001003535 \
   -dataset_id EGAD00001003536 \
@@ -39,3 +54,7 @@ perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.analyses.es.pl \
   -dataset_id EGAD00001003542 \
   -dataset_id EGAD00001003543 \
   -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
+
+
+
+
