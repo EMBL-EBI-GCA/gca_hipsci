988424e70e9c2a1e7aebaf6a794061ab2a8bc7af
apax18 <amirp@ebi.ac.uk>
2018-11-30 15:27:16 +0000

    fix ena_samples_issue

--- shell/elasticsearch_files/load_ena_runs.bash
+++ shell/elasticsearch_files/load_ena_runs.bash
@@ -7,14 +7,12 @@ HIPSCI_CODE=`dirname $0`/../..
 
 perl $HIPSCI_CODE/scripts/indexes/ena_index.runs.es.pl \
   $ERA_DB_ARGS \
+  -study_id ERP006946 \
+  -study_id ERP007111 \
+  -study_id ERP013436 \
+  -study_id ERP016335 \
   -study_id ERP017015 \
   -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
 
-# did not recognise sample SAMEA4939006, in study_id ERP017015
-#  -study_id ERP017015 \
-
-#Add these, removed just to test EPR017015
-#  -study_id ERP006946 \
-#  -study_id ERP007111 \
-#  -study_id ERP013436 \
-#  -study_id ERP016335 \
+# There were 5 unrecognised samples, 'SAMEA4939006', 'SAMEA4939007', 'SAMEA4939008', 'SAMEA4939009' and 'SAMEA4939010' associated
+# with study id ERP017015. These are ignored by ena_index.runs.es.pl and they need to be reviewed.
