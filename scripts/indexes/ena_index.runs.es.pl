e76914648bd33c0505e426c6d52ff0bce60d8198
apax18 <amirp@ebi.ac.uk>
2018-11-30 13:45:27 +0000

    fix ena - wgs

--- scripts/indexes/ena_index.runs.es.pl
+++ scripts/indexes/ena_index.runs.es.pl
@@ -74,7 +74,7 @@ foreach my $study_id (@study_id) {
   }
   die "did not recognise disease for $study_id" if !$disease;
   # print $row;
-  print Dumper($row);
+  # print Dumper($row);
   $sth_run->bind_param(1, $study_id);
   $sth_run->execute or die "could not execute";
 
@@ -86,120 +86,123 @@ foreach my $study_id (@study_id) {
     my $cgap_ips_line = List::Util::first {$_->biosample_id && $_->biosample_id eq $row->{BIOSAMPLE_ID}} @$cgap_ips_lines;
     my $cgap_tissue = $cgap_ips_line ? $cgap_ips_line->tissue
                     : List::Util::first {$_->biosample_id eq $row->{BIOSAMPLE_ID}} @$cgap_tissues;
+    if (!$cgap_tissue) {
+      print $row->{BIOSAMPLE_ID};
+    }
     die 'did not recognise sample '.$row->{BIOSAMPLE_ID} if !$cgap_tissue;
 
 
-    my $sample_name = $cgap_ips_line ? $cgap_ips_line->name : $cgap_tissue->name;
-    my $source_material = $cgap_tissue->tissue_type || '';
-    my $cell_type = $cgap_ips_line ? 'iPSC'
-                  : CORE::fc($source_material) eq CORE::fc('skin tissue') ? 'Fibroblast'
-                  : CORE::fc($source_material) eq CORE::fc('whole blood') ? 'PBMC'
-                  : die "did not recognise source material $source_material";
-
-    my $run_time = DateTime::Format::ISO8601->parse_datetime($row->{FIRST_CREATED})->subtract(days => 90);
-    my ($growing_conditions, $passage_number);
-    if ($cgap_ips_line) {
-      my $cgap_release = $cgap_ips_line->get_release_for(type => 'qc2', date =>$run_time->ymd);
-      if (!$cgap_release && $short_assay eq 'wgs') {
-        $cgap_release = $cgap_ips_line->get_release_for(type => 'qc1', date =>$run_time->ymd);
-      }
-      die "no qc2 cgap release for $sample_name" if !$cgap_release;
-      $growing_conditions = $cgap_release->is_feeder_free ? 'Feeder-free' : 'Feeder-dependent';
-      $passage_number = $cgap_release->passage;
-    }
-    else {
-      $growing_conditions = $cell_type;
-    }
-
-    my ($filename, $dirname) = fileparse($file->{filename});
-    my $file_description = 'Raw sequencing reads';
-
-    my $es_id = join('-', $sample_name, $short_assay, $row->{RUN_ID});
-    $es_id =~ s/\s/_/g;
-
-    $docs{$es_id} = {
-      description => 'Raw sequencing reads',
-      files => [
-        {
-          name => $filename,
-          md5 => $file->{checksum},
-          type => $file->{filetype},
-        }
-      ],
-      archive => {
-        name => 'ENA',
-        accession => $row->{RUN_ID},
-        accessionType => 'RUN_ID',
-        url => 'http://www.ebi.ac.uk/ena/data/view/'.$row->{RUN_ID},
-        ftpUrl => sprintf('ftp://ftp.sra.ebi.ac.uk/vol1/%s%s', $dirname, uri_escape($filename)),
-        openAccess => 1,
-      },
-      samples => [{
-        name => $sample_name,
-        bioSamplesAccession => $row->{BIOSAMPLE_ID},
-        cellType => $cell_type,
-        diseaseStatus => $disease,
-        sex => $cgap_tissue->donor->gender,
-        growingConditions => $growing_conditions,
-      }],
-      assay => {
-        type => $long_assay,
-        description => [ map {$_.'='.$row->{$_}}  qw(INSTRUMENT_PLATFORM INSTRUMENT_MODEL LIBRARY_LAYOUT LIBRARY_STRATEGY LIBRARY_SOURCE LIBRARY_SELECTION PAIRED_NOMINAL_LENGTH)],
-        instrument => $row->{INSTRUMENT_MODEL}
-      }
-    };
-    if ($passage_number) {
-      $docs{$es_id}{samples}[0]{passageNumber} = $passage_number;
-    }
-    if (my $exp_protocol = $experiment_xml_hash->{DESIGN}{LIBRARY_DESCRIPTOR}{LIBRARY_CONSTRUCTION_PROTOL}) {
-      push(@{$docs{$es_id}{assay}{description}}, $exp_protocol);
-    }
+    # my $sample_name = $cgap_ips_line ? $cgap_ips_line->name : $cgap_tissue->name;
+    # my $source_material = $cgap_tissue->tissue_type || '';
+    # my $cell_type = $cgap_ips_line ? 'iPSC'
+    #               : CORE::fc($source_material) eq CORE::fc('skin tissue') ? 'Fibroblast'
+    #               : CORE::fc($source_material) eq CORE::fc('whole blood') ? 'PBMC'
+    #               : die "did not recognise source material $source_material";
+    #
+    # my $run_time = DateTime::Format::ISO8601->parse_datetime($row->{FIRST_CREATED})->subtract(days => 90);
+    # my ($growing_conditions, $passage_number);
+    # if ($cgap_ips_line) {
+    #   my $cgap_release = $cgap_ips_line->get_release_for(type => 'qc2', date =>$run_time->ymd);
+    #   if (!$cgap_release && $short_assay eq 'wgs') {
+    #     $cgap_release = $cgap_ips_line->get_release_for(type => 'qc1', date =>$run_time->ymd);
+    #   }
+    #   die "no qc2 cgap release for $sample_name" if !$cgap_release;
+    #   $growing_conditions = $cgap_release->is_feeder_free ? 'Feeder-free' : 'Feeder-dependent';
+    #   $passage_number = $cgap_release->passage;
+    # }
+    # else {
+    #   $growing_conditions = $cell_type;
+    # }
+    #
+    # my ($filename, $dirname) = fileparse($file->{filename});
+    # my $file_description = 'Raw sequencing reads';
+    #
+    # my $es_id = join('-', $sample_name, $short_assay, $row->{RUN_ID});
+    # $es_id =~ s/\s/_/g;
+    #
+    # $docs{$es_id} = {
+    #   description => 'Raw sequencing reads',
+    #   files => [
+    #     {
+    #       name => $filename,
+    #       md5 => $file->{checksum},
+    #       type => $file->{filetype},
+    #     }
+    #   ],
+    #   archive => {
+    #     name => 'ENA',
+    #     accession => $row->{RUN_ID},
+    #     accessionType => 'RUN_ID',
+    #     url => 'http://www.ebi.ac.uk/ena/data/view/'.$row->{RUN_ID},
+    #     ftpUrl => sprintf('ftp://ftp.sra.ebi.ac.uk/vol1/%s%s', $dirname, uri_escape($filename)),
+    #     openAccess => 1,
+    #   },
+    #   samples => [{
+    #     name => $sample_name,
+    #     bioSamplesAccession => $row->{BIOSAMPLE_ID},
+    #     cellType => $cell_type,
+    #     diseaseStatus => $disease,
+    #     sex => $cgap_tissue->donor->gender,
+    #     growingConditions => $growing_conditions,
+    #   }],
+    #   assay => {
+    #     type => $long_assay,
+    #     description => [ map {$_.'='.$row->{$_}}  qw(INSTRUMENT_PLATFORM INSTRUMENT_MODEL LIBRARY_LAYOUT LIBRARY_STRATEGY LIBRARY_SOURCE LIBRARY_SELECTION PAIRED_NOMINAL_LENGTH)],
+    #     instrument => $row->{INSTRUMENT_MODEL}
+    #   }
+    # };
+    # if ($passage_number) {
+    #   $docs{$es_id}{samples}[0]{passageNumber} = $passage_number;
+    # }
+    # if (my $exp_protocol = $experiment_xml_hash->{DESIGN}{LIBRARY_DESCRIPTOR}{LIBRARY_CONSTRUCTION_PROTOL}) {
+    #   push(@{$docs{$es_id}{assay}{description}}, $exp_protocol);
+    # }
   }
 
 }
-
-my $scroll = $elasticsearch->call('scroll_helper', (
-  index => 'hipsci',
-  type => 'file',
-  search_type => 'scan',
-  size => 500,
-  body => {
-    query => {
-      filtered => {
-        filter => {
-          and => [
-            {term => {
-              'archive.name' => 'ENA',
-            }},
-            {term => {
-              'file.accessionType' => 'RUN_ID',
-            }}
-          ]
-        }
-      }
-    }
-  }
-));
-
-my $date = strftime('%Y%m%d', localtime);
-ES_DOC:
-while (my $es_doc = $scroll->next) {
-  my $new_doc = $docs{$es_doc->{_id}};
-  if (!$new_doc) {
-    printf("curl -XDELETE http://%s/%s/%s/%s\n", $es_host, @$es_doc{qw(_index _type _id)});
-    next ES_DOC;
-  }
-  delete $docs{$es_doc->{_id}};
-  my ($created, $updated) = @{$es_doc->{_source}}{qw(_indexCreated _indexUpdated)};
-  $new_doc->{_indexCreated} = $es_doc->{_source}{_indexCreated} || $date;
-  $new_doc->{_indexUpdated} = $es_doc->{_source}{_indexUpdated} || $date;
-  next ES_DOC if Compare($new_doc, $es_doc->{_source});
-  $new_doc->{_indexUpdated} = $date;
-  $elasticsearch->index_file(id => $es_doc->{_id}, body => $new_doc);
-}
-while (my ($es_id, $new_doc) = each %docs) {
-  $new_doc->{_indexCreated} = $date;
-  $new_doc->{_indexUpdated} = $date;
-  $elasticsearch->index_file(body => $new_doc, id => $es_id);
-}
-
+#
+# my $scroll = $elasticsearch->call('scroll_helper', (
+#   index => 'hipsci',
+#   type => 'file',
+#   search_type => 'scan',
+#   size => 500,
+#   body => {
+#     query => {
+#       filtered => {
+#         filter => {
+#           and => [
+#             {term => {
+#               'archive.name' => 'ENA',
+#             }},
+#             {term => {
+#               'file.accessionType' => 'RUN_ID',
+#             }}
+#           ]
+#         }
+#       }
+#     }
+#   }
+# ));
+#
+# my $date = strftime('%Y%m%d', localtime);
+# ES_DOC:
+# while (my $es_doc = $scroll->next) {
+#   my $new_doc = $docs{$es_doc->{_id}};
+#   if (!$new_doc) {
+#     printf("curl -XDELETE http://%s/%s/%s/%s\n", $es_host, @$es_doc{qw(_index _type _id)});
+#     next ES_DOC;
+#   }
+#   delete $docs{$es_doc->{_id}};
+#   my ($created, $updated) = @{$es_doc->{_source}}{qw(_indexCreated _indexUpdated)};
+#   $new_doc->{_indexCreated} = $es_doc->{_source}{_indexCreated} || $date;
+#   $new_doc->{_indexUpdated} = $es_doc->{_source}{_indexUpdated} || $date;
+#   next ES_DOC if Compare($new_doc, $es_doc->{_source});
+#   $new_doc->{_indexUpdated} = $date;
+#   $elasticsearch->index_file(id => $es_doc->{_id}, body => $new_doc);
+# }
+# while (my ($es_id, $new_doc) = each %docs) {
+#   $new_doc->{_indexCreated} = $date;
+#   $new_doc->{_indexUpdated} = $date;
+#   $elasticsearch->index_file(body => $new_doc, id => $es_id);
+# }
+#
