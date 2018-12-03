448d150395be3d85906b407d30d16f47eaa329c5
apax18 <amirp@ebi.ac.uk>
2018-12-03 15:41:19 +0000

    add new ega dataset - sequencing

--- shell/elasticsearch_files/load_ega_analyses.bash
+++ shell/elasticsearch_files/load_ega_analyses.bash
@@ -52,12 +52,16 @@ perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.analyses.es.pl \
   -dataset_id EGAD00001003541 \
   -dataset_id EGAD00001003542 \
   -dataset_id EGAD00001003543 \
+  -dataset_id EGAD00001003204 \
+  -dataset_id EGAD00001002235 \
+  -dataset_id EGAD00001004266 \
+  -dataset_id EGAD00001004295 \
   -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
-
-
 
 #  -dataset_id EGAD00001001999 \
 #  -dataset_id EGAD00001002000 \
+#This is the last one that was already here:
+#  -dataset_id EGAD00001003543 \
 
 
 
