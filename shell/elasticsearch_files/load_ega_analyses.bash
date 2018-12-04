ae1d83d1624897f8f6e1182bf0e02c46c31364ad
apax18 <amirp@ebi.ac.uk>
2018-12-04 13:36:30 +0000

    ega_new_data

--- shell/elasticsearch_files/load_ega_analyses.bash
+++ shell/elasticsearch_files/load_ega_analyses.bash
@@ -52,7 +52,6 @@ perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.analyses.es.pl \
   -dataset_id EGAD00001003541 \
   -dataset_id EGAD00001003542 \
   -dataset_id EGAD00001003543 \
-  -dataset_id EGAD00001003204 \
   -dataset_id EGAD00001002235 \
   -dataset_id EGAD00001004266 \
   -dataset_id EGAD00001004295 \
