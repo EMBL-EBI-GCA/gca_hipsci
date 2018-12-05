438c75a08031fcf1e7dcdabbb57c0b273a93f994
apax18 <amirp@ebi.ac.uk>
2018-12-05 13:38:54 +0000

    new ega

--- scripts/indexes/ega_dataset_index.runs.es.pl
+++ scripts/indexes/ega_dataset_index.runs.es.pl
@@ -87,7 +87,7 @@ foreach my $dataset_id (@dataset_id) {
       # print $row->{BIOSAMPLE_ID}; # returns biosample id for the ones with error even.
       # print $_;
       # print Dumper($cgap_ips_lines);
-      print @$cgap_ips_lines;
+      print Dumper(@$cgap_ips_lines);
       last;
 
     my $cgap_ips_line = List::Util::first {$_->biosample_id && $_->biosample_id eq $row->{BIOSAMPLE_ID}} @$cgap_ips_lines;
