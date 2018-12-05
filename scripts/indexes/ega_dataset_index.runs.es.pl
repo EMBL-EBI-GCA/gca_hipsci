19e89b737453392de7d83c07cfd830c1aa93ff3f
apax18 <amirp@ebi.ac.uk>
2018-12-05 11:32:11 +0000

    new ega

--- scripts/indexes/ega_dataset_index.runs.es.pl
+++ scripts/indexes/ega_dataset_index.runs.es.pl
@@ -83,7 +83,8 @@ foreach my $dataset_id (@dataset_id) {
     my $experiment_xml_hash = XMLin($row->{EXPERIMENT_XML});
     my $file = $xml_hash->{RUN}{DATA_BLOCK}{FILES}{FILE};
       # print Dumper($file);
-      print $row->{BIOSAMPLE_ID};
+      # print $row->{BIOSAMPLE_ID}; # returns biosample id for the ones with error even.
+      print $_->biosample_id;
     my $cgap_ips_line = List::Util::first {$_->biosample_id && $_->biosample_id eq $row->{BIOSAMPLE_ID}} @$cgap_ips_lines;
       # print Dumper($cgap_ips_line);
       my $cgap_tissue = $cgap_ips_line ? $cgap_ips_line->tissue
