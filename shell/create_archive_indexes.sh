0ff23c5156055d56b78ead42341501ba8cc2b9b1
apax18 <amirp@ebi.ac.uk>
2018-12-06 14:59:46 +0000

    create archive indexes

--- shell/create_archive_indexes.sh
+++ shell/create_archive_indexes.sh
@@ -18,12 +18,12 @@ perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.runs.pl \
   -dataset_id EGAD00001001438 \
   -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
 
-#perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.analyses.pl \
-#  $ERA_DB_ARGS \
-#  -dataset_id EGAD00001001437 \
-#  -dataset_id EGAD00001001438 \
-#  -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
-#
+perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.analyses.pl \
+  $ERA_DB_ARGS \
+  -dataset_id EGAD00001001437 \
+  -dataset_id EGAD00001001438 \
+  -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
+
 #perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.arrays.pl \
 #  $ERA_DB_ARGS \
 #  -dataset EGAD00010000773=/nfs/research1/hipsci/tracking_resources/ega_array_data_submissions/EGAS00001000866.gtarray.201411.tsv \
