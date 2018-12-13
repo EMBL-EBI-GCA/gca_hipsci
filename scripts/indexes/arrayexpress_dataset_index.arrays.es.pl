92e44a64b57b119eb2dbf77eca6d5c67bd1085ff
apax18 <amirp@ebi.ac.uk>
2018-12-13 14:00:20 +0000

    new array express

--- scripts/indexes/arrayexpress_dataset_index.arrays.es.pl
+++ scripts/indexes/arrayexpress_dataset_index.arrays.es.pl
@@ -112,7 +112,7 @@ foreach my $dataset_id (@dataset_id) { # E-MTAB-4057, E-MTAB-4059, E-MTAB-4748
       my @parts = split("\t", $line);
       my $full_cellline = $parts[$column_of{"Assay Name"}];  # works, HGII_16432_1, HGII_16366_1 ...
       my @line_array = split('\.', $full_cellline);
-      print "@line_array\n";
+      print "@line_array";
       my $cellline = $line_array[0];
       my $mt_ftp_link = $parts[$column_of{"Comment [Derived ArrayExpress FTP file]"}];
       my $mt_file = $parts[$column_of{"Derived Array Data File"}];
