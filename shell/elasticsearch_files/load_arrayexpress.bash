bf73100c394da7498fe21fd5725b6c70f9a31328
apax18 <amirp@ebi.ac.uk>
2018-12-13 10:53:37 +0000

    new array express

--- shell/elasticsearch_files/load_arrayexpress.bash
+++ shell/elasticsearch_files/load_arrayexpress.bash
@@ -6,9 +6,12 @@ HIPSCI_CODE=`dirname $0`/../..
 # Add any new ids before running this script.
 
 perl $HIPSCI_CODE/scripts/indexes/arrayexpress_dataset_index.arrays.es.pl \
-  -dataset_id E-MTAB-4057 \
-  -dataset_id E-MTAB-4059 \
+  -dataset_id E-MTAB-7032 \
   -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
 
-  # -dataset_id E-MTAB-7032 \ # doesn't recognise this. 
+  # -dataset_id E-MTAB-7032 \ # needs to be added.
   # -dataset_id E-MTAB-4748 \ # doesn't recognise this.
+
+#existing ones:
+#  -dataset_id E-MTAB-4057 \
+#  -dataset_id E-MTAB-4059 \
