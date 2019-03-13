d5f129cef13a1bfe746379d0e786e451f1ef2b90
apax18 <amirp@ebi.ac.uk>
2019-03-13 10:38:10 +0000

    test

--- scripts/indexes/IDR_dataset.es.pl
+++ scripts/indexes/IDR_dataset.es.pl
@@ -86,11 +86,13 @@ my ($cgap_ips_lines, $cgap_tissues, $cgap_donors) =  @{read_cgap_report()}{qw(ip
 improve_donors(donors=>$cgap_donors, demographic_file=>$demographic_filename); # improve_donors method
 
 my (%cgap_ips_line_hash, %cgap_tissues_hash);
-print Dumper(%cgap_ips_line_hash);
-# foreach my $cell_line (@$cgap_ips_lines) {
-#   $cgap_ips_line_hash{$cell_line->name} = $cell_line;
-#   $cgap_tissues_hash{$cell_line->tissue->name} = $cell_line->tissue;
-# }
+# print Dumper(%cgap_ips_line_hash);    # uninitialized still.
+foreach my $cell_line (@$cgap_ips_lines) {   # for each cellline in this array (dereferencing an array ref)
+  $cgap_ips_line_hash{$cell_line->name} = $cell_line;
+  $cgap_tissues_hash{$cell_line->tissue->name} = $cell_line->tissue;
+  print Dumper($cgap_ips_line_hash);
+  last;
+}
 # print Dumper(%cgap_ips_line_hash);
 # my $date = '20180831';
 # my $label = 'vep_openaccess_bcf';
