7b3710fde46f57c7ae6e52243597336d6b9f6db0
apax18 <amirp@ebi.ac.uk>
2019-03-25 16:54:38 +0000

    test

--- scripts/indexes/IDR.es.pl
+++ scripts/indexes/IDR.es.pl
@@ -35,7 +35,7 @@ open my $fh, '<', $json_file or die "could not open $json_file $!";
 # print Dumper($fh);
 
 my $data_structure = decode_json($fh);
-print Dumper($data_structure);
+# print Dumper($data_structure);
 
 #
 # #  NEW
@@ -140,3 +140,22 @@ print Dumper($data_structure);
 #     }
 # }
 # }
+use lib qw(..);
+
+use JSON qw( );
+
+my $filename = '/homes/hipdcc/IDR_data/IDR_json_data.json';
+
+my $json_text = do {
+   open(my $json_fh, "<:encoding(UTF-8)", $filename)
+      or die("Can't open \$filename\": $!\n");
+   local $/;
+   <$json_fh>
+};
+
+my $json = JSON->new;
+my $data = $json->decode($json_text);
+
+for ( @{$data->{experiment_2}} ) {
+   print $_->{Accession}."\n";
+}
