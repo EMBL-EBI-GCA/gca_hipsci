ba4259e457bffde93dbd8fdccbd69189c7e83169
apax18 <amirp@ebi.ac.uk>
2019-03-28 15:45:56 +0000

    last version of IDR data load

--- scripts/indexes/idr_data.es.pl
+++ scripts/indexes/idr_data.es.pl
@@ -52,7 +52,7 @@ foreach my $exp (@experiment_array) {
     $docs{$es_id} = {
         description => $description,
         files => [{
-            name => $exp,
+            name => $IDR_No + '-' + $exp,
         }],
         archive     => {
             name       => 'IDR',
