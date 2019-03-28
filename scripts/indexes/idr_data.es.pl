12756ef83f181dcf1d226a6f4e87737eec943c75
apax18 <amirp@ebi.ac.uk>
2019-03-28 17:25:06 +0000

    last version of IDR data load

--- scripts/indexes/idr_data.es.pl
+++ scripts/indexes/idr_data.es.pl
@@ -22,12 +22,11 @@ my $demographic_filename;
 my $description = 'High content fluorescence microscopy';
 
 # below two variables need to be updated accordingly based on each specific IDR:
-my $filename = '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_2051.json';  # IDR json data file
-my $IDR_No = 'idr0037-vigilante-hipsci/screenA'; # IDR file name
-
-# for IDR0034: '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_1901.json' and 'idr0034-kilpinen-hipsci/screenA'
-# my $filename = '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_1901.json';  # IDR json data file
-# my $IDR_No = 'idr0034-kilpinen-hipsci/screenA'; # IDR file name
+# my $filename = '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_2051.json';  # IDR json data file
+# my $IDR_No = 'idr0037-vigilante-hipsci/screenA'; # IDR file name
+my $filename = '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_1901.json';  # IDR json data file
+my $IDR_No = 'idr0034-kilpinen-hipsci/screenA'; # IDR file name
+# for IDR0034: '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_1901.json' and 'idr0034-kilpinen-hipsci/screenA';
 
 my $elasticsearch = ReseqTrack::Tools::HipSci::ElasticsearchClient->new(host => $es_host);
 my ($cgap_ips_lines, $cgap_tissues, $cgap_donors) =  @{read_cgap_report()}{qw(ips_lines tissues donors)};
@@ -46,7 +45,7 @@ my $json_text = do {
 my $json = JSON->new;
 my $data = $json->decode($json_text);
 my @experiment_array = keys %$data;
-print Dumper($data);
+
 my %docs;
 FILE:
 foreach my $exp (@experiment_array) {
@@ -74,46 +73,45 @@ foreach my $exp (@experiment_array) {
             description => [ 'SOFTWARE=SNP2HLA', 'PLATFORM=Illumina beadchip HumanCoreExome-12' ],
             instrument  => 'Operetta',
         }
-    },
-    print Dumper($docs{$es_id});
+    }
 }
-#
-# my $scroll = $elasticsearch->call('scroll_helper', (
-#   index => 'hipsci',
-#   type => 'file',
-#   search_type => 'scan',
-#   size => 500,
-#   body => {
-#     query => {
-#       filtered => {
-#         filter => {
-#           term => {
-#             description => $description
-#           },
-#         }
-#       }
-#     }
-#   }
-# ));
-#
-# my $systemdate = strftime('%Y%m%d', localtime);
-# ES_DOC:
-# while (my $es_doc = $scroll->next) {
-#   my $new_doc = $docs{$es_doc->{_id}};
-#   if (!$new_doc) {
-#     printf("curl -XDELETE http://%s/%s/%s/%s\n", $es_host, @$es_doc{qw(_index _type _id)});
-#     next ES_DOC;
-#   }
-#   delete $docs{$es_doc->{_id}};
-#   my ($created, $updated) = @{$es_doc->{_source}}{qw(_indexCreated _indexUpdated)};
-#   $new_doc->{_indexCreated} = $es_doc->{_source}{_indexCreated} || $systemdate;
-#   $new_doc->{_indexUpdated} = $es_doc->{_source}{_indexUpdated} || $systemdate;
-#   next ES_DOC if Compare($new_doc, $es_doc->{_source});
-#   $new_doc->{_indexUpdated} = $systemdate;
-#   $elasticsearch->index_file(id => $es_doc->{_id}, body => $new_doc);
-# }
-# while (my ($es_id, $new_doc) = each %docs) {
-#   $new_doc->{_indexCreated} = $systemdate;
-#   $new_doc->{_indexUpdated} = $systemdate;
-#   $elasticsearch->index_file(body => $new_doc, id => $es_id);
-# }
+
+my $scroll = $elasticsearch->call('scroll_helper', (
+  index => 'hipsci',
+  type => 'file',
+  search_type => 'scan',
+  size => 500,
+  body => {
+    query => {
+      filtered => {
+        filter => {
+          term => {
+            description => $description
+          },
+        }
+      }
+    }
+  }
+));
+
+my $systemdate = strftime('%Y%m%d', localtime);
+ES_DOC:
+while (my $es_doc = $scroll->next) {
+  my $new_doc = $docs{$es_doc->{_id}};
+  if (!$new_doc) {
+    printf("curl -XDELETE http://%s/%s/%s/%s\n", $es_host, @$es_doc{qw(_index _type _id)});
+    next ES_DOC;
+  }
+  delete $docs{$es_doc->{_id}};
+  my ($created, $updated) = @{$es_doc->{_source}}{qw(_indexCreated _indexUpdated)};
+  $new_doc->{_indexCreated} = $es_doc->{_source}{_indexCreated} || $systemdate;
+  $new_doc->{_indexUpdated} = $es_doc->{_source}{_indexUpdated} || $systemdate;
+  next ES_DOC if Compare($new_doc, $es_doc->{_source});
+  $new_doc->{_indexUpdated} = $systemdate;
+  $elasticsearch->index_file(id => $es_doc->{_id}, body => $new_doc);
+}
+while (my ($es_id, $new_doc) = each %docs) {
+  $new_doc->{_indexCreated} = $systemdate;
+  $new_doc->{_indexUpdated} = $systemdate;
+  $elasticsearch->index_file(body => $new_doc, id => $es_id);
+}
