295586996f1599d25f8eba3e4d7c5a372cd57232
apax18 <amirp@ebi.ac.uk>
2019-04-02 16:18:55 +0100

    test celltype diseasestatus

--- scripts/indexes/IDR.es.pl
+++ scripts/indexes/IDR.es.pl
@@ -76,10 +76,12 @@ foreach my $cell_line (@IDR_celllines) {
     my $content = $browser->content();
     my $json = new JSON;
     my $json_text = $json->decode($content);
-    foreach my $record (@{$json_text->{hits}{hits}}) {
-        print Dumper($cell_line);
-        print Dumper($record->{_source}{samples}[0]{cellType});
-    }
+    print Dumper ($json_text);
+    last;
+    # foreach my $record (@{$json_text->{hits}{hits}}) {
+    #     print Dumper($cell_line);
+    #     print Dumper($record->{_source}{samples}[0]{cellType});
+    # }
 }
 # print Dumper(@IDR_celllines);
 #### this is the only bit we haven't prepared:
