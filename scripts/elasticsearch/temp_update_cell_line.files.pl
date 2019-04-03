7c7b7ebd8852033144eca1e9e51914ad146e89fc
apax18 <amirp@ebi.ac.uk>
2019-04-03 16:36:21 +0100

    IDR in Assay in cell line page

--- scripts/elasticsearch/temp_update_cell_line.files.pl
+++ scripts/elasticsearch/temp_update_cell_line.files.pl
@@ -59,36 +59,37 @@ while(1) {
 my %elasticsearch;
 foreach my $es_host (@es_host){
   $elasticsearch{$es_host} = ReseqTrack::Tools::HipSci::ElasticsearchClient->new(host => $es_host);
-  print DUmper($elasticsearch{$es_host});
+  print %elasticsearch{$es_host};
 }
 
-# my $scroll = $elasticsearch{$es_host[0]}->call('scroll_helper',
-#   index       => 'hipsci',
-#   type        => 'file',
-#   search_type => 'scan',
-#   size        => 500
-# );
-#
-# my %ontology_map = (
-#   'Proteomics' => 'http://www.ebi.ac.uk/efo/EFO_0002766',
-#   'Genotyping array' => 'http://www.ebi.ac.uk/efo/EFO_0002767',
-#   'RNA-seq' => 'http://www.ebi.ac.uk/efo/EFO_0002770',
-#   'Cellular phenotyping' => 'http://www.ebi.ac.uk/efo/EFO_0005399',
-#   'Methylation array' => 'http://www.ebi.ac.uk/efo/EFO_0002759',
-#   'Expression array' => 'http://www.ebi.ac.uk/efo/EFO_0002770',
-#   'Exome-seq' => 'http://www.ebi.ac.uk/efo/EFO_0005396',
-#   'ChIP-seq' => 'http://www.ebi.ac.uk/efo/EFO_0002692',
-#   'Whole genome sequencing' => 'http://www.ebi.ac.uk/efo/EFO_0003744',
-# );
-# my %cell_line_assays;
-# while ( my $doc = $scroll->next ) {
-#   my $assay = $doc->{_source}{assay}{type};
-#   SAMPLE:
-#   foreach my $sample (@{$$doc{'_source'}{'samples'}}){
-#     $cell_line_assays{$sample->{name}}{$assay} = {name => $assay, ontologyPURL => $ontology_map{$assay}};
-#   }
-# }
-#
+my $scroll = $elasticsearch{$es_host[0]}->call('scroll_helper',
+  index       => 'hipsci',
+  type        => 'file',
+  search_type => 'scan',
+  size        => 500
+);
+
+my %ontology_map = (
+    'Proteomics'              => 'http://www.ebi.ac.uk/efo/EFO_0002766',
+    'Genotyping array'        => 'http://www.ebi.ac.uk/efo/EFO_0002767',
+    'RNA-seq'                 => 'http://www.ebi.ac.uk/efo/EFO_0002770',
+    'Cellular phenotyping'    => 'http://www.ebi.ac.uk/efo/EFO_0005399',
+    'Methylation array'       => 'http://www.ebi.ac.uk/efo/EFO_0002759',
+    'Expression array'        => 'http://www.ebi.ac.uk/efo/EFO_0002770',
+    'Exome-seq'               => 'http://www.ebi.ac.uk/efo/EFO_0005396',
+    'ChIP-seq'                => 'http://www.ebi.ac.uk/efo/EFO_0002692',
+    'Whole genome sequencing' => 'http://www.ebi.ac.uk/efo/EFO_0003744',
+    'High content imaging'    => 'http://www.ebi.ac.uk/efo/EFO_0007550',
+);
+my %cell_line_assays;
+while ( my $doc = $scroll->next ) {
+  my $assay = $doc->{_source}{assay}{type};
+  SAMPLE:
+  foreach my $sample (@{$$doc{'_source'}{'samples'}}){
+    $cell_line_assays{$sample->{name}}{$assay} = {name => $assay, ontologyPURL => $ontology_map{$assay}};
+  }
+}
+
 # LINE:
 # foreach my $epd_line (@$epd_lines) {
 #   my $short_name = $epd_line->{label};
