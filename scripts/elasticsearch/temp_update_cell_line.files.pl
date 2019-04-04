5d8810d6229a525b7f7b4ad888b6572931c24d9b
apax18 <amirp@ebi.ac.uk>
2019-04-04 15:13:06 +0100

    IDR in Assay in cell line page

--- scripts/elasticsearch/temp_update_cell_line.files.pl
+++ scripts/elasticsearch/temp_update_cell_line.files.pl
@@ -23,8 +23,8 @@ my $idr_find_url = 'https://idr.openmicroscopy.org/mapr/api/cellline/?orphaned=t
 my $idr_link_url = 'https://idr.openmicroscopy.org/mapr/cellline/?value=%s';
 my $es_host='ves-hx-e3:9200';
 
-my $filename = '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_1901.json';  # IDR json file for 'idr0034-kilpinen-hipsci/screenA'
-# my $filename = '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_2051.json';  # IDR json file for 'idr0037-vigilante-hipsci/screenA'
+# my $filename = '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_1901.json';  # IDR json file for 'idr0034-kilpinen-hipsci/screenA'
+my $filename = '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_2051.json';  # IDR json file for 'idr0037-vigilante-hipsci/screenA'
 
 my $json_text = do {
    open(my $json_fh, "<:encoding(UTF-8)", $filename)
@@ -44,13 +44,13 @@ foreach my $experiment (@experiment_array) {
    }
 }
 print Dumper(@IDR_celllines);
-# # $VAR60 = 'HPSI0713i-qimz_1';
-# # $VAR61 = 'HPSI0713i-darw_2';
-# # $VAR62 = 'HPSI0613i-auim_2';
-# # &GetOptions(
-# #   'es_host=s' =>\@es_host,
-# # );
-#
+# $VAR60 = 'HPSI0713i-qimz_1';
+# $VAR61 = 'HPSI0713i-darw_2';
+# $VAR62 = 'HPSI0613i-auim_2';
+# &GetOptions(
+#   'es_host=s' =>\@es_host,
+# );
+
 # my $epd_content = LWP::Simple::get($epd_find_url);
 # die "error getting $epd_find_url" if !defined $epd_content;
 # my $epd_lines = JSON::decode_json($epd_content);
@@ -74,49 +74,49 @@ print Dumper(@IDR_celllines);
 #   last IDR_PAGE if ! scalar @{$idr_lines->{maps}};
 #   push(@idr_lines, grep {/^HPSI/} map {$_->{id}} @{$idr_lines->{maps}});
 # }
-# # print Dumper(@idr_lines);
-# # $VAR23 = 'HPSI0513i-cuau_1';
-# # $VAR24 = 'HPSI0513i-euir_2';
-# # $VAR25 = 'HPSI0613i-riiv_3';
-#
-#
-#
-# # my %elasticsearch;
-# # foreach my $es_host (@es_host){
-# #   $elasticsearch{$es_host} = ReseqTrack::Tools::HipSci::ElasticsearchClient->new(host => $es_host);
-# #   # print Dumper($elasticsearch);  !!!!!
-# # }
-# my $elasticsearch = ReseqTrack::Tools::HipSci::ElasticsearchClient->new(host => $es_host);
-#
-# # my $scroll = $elasticsearch{$es_host[0]}->call('scroll_helper',
-# my $scroll = $elasticsearch->call('scroll_helper',
-#   index       => 'hipsci',
-#   type        => 'file',
-#   search_type => 'scan',
-#   size        => 500
-# );
-#
-# my %ontology_map = (
-#     'Proteomics'              => 'http://www.ebi.ac.uk/efo/EFO_0002766',
-#     'Genotyping array'        => 'http://www.ebi.ac.uk/efo/EFO_0002767',
-#     'RNA-seq'                 => 'http://www.ebi.ac.uk/efo/EFO_0002770',
-#     'Cellular phenotyping'    => 'http://www.ebi.ac.uk/efo/EFO_0005399',
-#     'Methylation array'       => 'http://www.ebi.ac.uk/efo/EFO_0002759',
-#     'Expression array'        => 'http://www.ebi.ac.uk/efo/EFO_0002770',
-#     'Exome-seq'               => 'http://www.ebi.ac.uk/efo/EFO_0005396',
-#     'ChIP-seq'                => 'http://www.ebi.ac.uk/efo/EFO_0002692',
-#     'Whole genome sequencing' => 'http://www.ebi.ac.uk/efo/EFO_0003744',
-#     'High content imaging'    => 'http://www.ebi.ac.uk/efo/EFO_0007550',
-# );
-# my %cell_line_assays;
-# while ( my $doc = $scroll->next ) {
-#   my $assay = $doc->{_source}{assay}{type};
-#   SAMPLE:
-#   foreach my $sample (@{$$doc{'_source'}{'samples'}}){
-#     $cell_line_assays{$sample->{name}}{$assay} = {name => $assay, ontologyPURL => $ontology_map{$assay}};
-#   }
+# print Dumper(@idr_lines);
+# $VAR23 = 'HPSI0513i-cuau_1';
+# $VAR24 = 'HPSI0513i-euir_2';
+# $VAR25 = 'HPSI0613i-riiv_3';
+
+
+
+# my %elasticsearch;
+# foreach my $es_host (@es_host){
+#   $elasticsearch{$es_host} = ReseqTrack::Tools::HipSci::ElasticsearchClient->new(host => $es_host);
+#   # print Dumper($elasticsearch);  !!!!!
 # }
-# #
+my $elasticsearch = ReseqTrack::Tools::HipSci::ElasticsearchClient->new(host => $es_host);
+
+# my $scroll = $elasticsearch{$es_host[0]}->call('scroll_helper',
+my $scroll = $elasticsearch->call('scroll_helper',
+  index       => 'hipsci',
+  type        => 'file',
+  search_type => 'scan',
+  size        => 500
+);
+
+my %ontology_map = (
+    'Proteomics'              => 'http://www.ebi.ac.uk/efo/EFO_0002766',
+    'Genotyping array'        => 'http://www.ebi.ac.uk/efo/EFO_0002767',
+    'RNA-seq'                 => 'http://www.ebi.ac.uk/efo/EFO_0002770',
+    'Cellular phenotyping'    => 'http://www.ebi.ac.uk/efo/EFO_0005399',
+    'Methylation array'       => 'http://www.ebi.ac.uk/efo/EFO_0002759',
+    'Expression array'        => 'http://www.ebi.ac.uk/efo/EFO_0002770',
+    'Exome-seq'               => 'http://www.ebi.ac.uk/efo/EFO_0005396',
+    'ChIP-seq'                => 'http://www.ebi.ac.uk/efo/EFO_0002692',
+    'Whole genome sequencing' => 'http://www.ebi.ac.uk/efo/EFO_0003744',
+    'High content imaging'    => 'http://www.ebi.ac.uk/efo/EFO_0007550',
+);
+my %cell_line_assays;
+while ( my $doc = $scroll->next ) {
+  my $assay = $doc->{_source}{assay}{type};
+  SAMPLE:
+  foreach my $sample (@{$$doc{'_source'}{'samples'}}){
+    $cell_line_assays{$sample->{name}}{$assay} = {name => $assay, ontologyPURL => $ontology_map{$assay}};
+  }
+}
+#
 # LINE:
 # foreach my $epd_line (@$epd_lines) {
 #   my $short_name = $epd_line->{label};
@@ -144,39 +144,40 @@ print Dumper(@IDR_celllines);
 #       idrURL => sprintf($idr_link_url, $idr_line),
 #     };
 # }
+
+LINE:
+foreach my $idr (@IDR_celllines) {
+  $cell_line_assays{$idr}{'High content imaging'} = {
+      name => 'High content imaging',
+      ontologyPURL =>$ontology_map{'High content imaging'},
+      # idrURL => sprintf($idr_link_url, $idr),
+    };
+}
+
 #
-# LINE:
-# foreach my $idr (@IDR_celllines) {
-#   $cell_line_assays{$idr}{'High content imaging'} = {
-#       name => 'High content imaging',
-#       ontologyPURL =>$ontology_map{'High content imaging'},
-#       # idrURL => sprintf($idr_link_url, $idr),
-#     };
+# while( my( $host, $elasticsearchserver ) = each %elasticsearch ){
+#   my $cell_updated = 0;
+#   my $cell_uptodate = 0;
+my $new_scroll = $elasticsearch->call('scroll_helper',
+  index       => 'hipsci',
+  type        => 'cellLine',
+  search_type => 'scan',
+  size        => 500
+);
+
+CELL_LINE:
+while ( my $doc = $new_scroll->next ) {
+  my $cell_line  = $doc->{_source}{name};
+  my @new_assays = values %{$cell_line_assays{$cell_line}};
+  next CELL_LINE if Compare(\@new_assays, $doc->{_source}{assays} || []);
+  if (scalar @new_assays) {
+    print Dumper(@new_assays);
+    $doc->{_source}{assays} = \@new_assays;
+  }
+  else {
+    delete $doc->{_source}{assays};
+  }
+  # $doc->{_source}{_indexUpdated} = $date;
+  # $elasticsearch->index_line(id => $doc->{_source}{name}, body => $doc->{_source});
+}
 # }
-#
-# #
-# # while( my( $host, $elasticsearchserver ) = each %elasticsearch ){
-# #   my $cell_updated = 0;
-# #   my $cell_uptodate = 0;
-# my $new_scroll = $elasticsearch->call('scroll_helper',
-#   index       => 'hipsci',
-#   type        => 'cellLine',
-#   search_type => 'scan',
-#   size        => 500
-# );
-#
-# CELL_LINE:
-# while ( my $doc = $new_scroll->next ) {
-#   my $cell_line  = $doc->{_source}{name};
-#   my @new_assays = values %{$cell_line_assays{$cell_line}};
-#   next CELL_LINE if Compare(\@new_assays, $doc->{_source}{assays} || []);
-#   if (scalar @new_assays) {
-#     $doc->{_source}{assays} = \@new_assays;
-#   }
-#   else {
-#     delete $doc->{_source}{assays};
-#   }
-#   $doc->{_source}{_indexUpdated} = $date;
-#   $elasticsearch->index_line(id => $doc->{_source}{name}, body => $doc->{_source});
-# }
-# # }
