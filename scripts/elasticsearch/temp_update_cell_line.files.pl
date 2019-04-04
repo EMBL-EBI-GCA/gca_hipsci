c8283255e62de1781bb480005d34cd14f81b2999
apax18 <amirp@ebi.ac.uk>
2019-04-04 13:27:17 +0100

    IDR in Assay in cell line page

--- scripts/elasticsearch/temp_update_cell_line.files.pl
+++ scripts/elasticsearch/temp_update_cell_line.files.pl
@@ -26,6 +26,14 @@ my $es_host='ves-hx-e3:9200';
 my $filename = '/homes/hipdcc/IDR_data/IDR_data/IDR_Screen_ID_1901.json';  # IDR json data file
 my $IDR_No = 'idr0034-kilpinen-hipsci/screenA'; # IDR file name
 
+my $json_text = do {
+   open(my $json_fh, "<:encoding(UTF-8)", $filename)
+      or die("Can't open \$filename\": $!\n");
+   local $/;
+   <$json_fh>
+};
+my $json = JSON->new;
+my $data = $json->decode($json_text);
 my @IDR_celllines; # cellline for the particular IDR like idr0034
 my @experiment_array = keys %$data;
 foreach my $experiment (@experiment_array) {
