9b9e3d52fa956968dbdd4665c076860f4d078826
apax18 <amirp@ebi.ac.uk>
2019-04-16 13:41:25 +0100

    new exome-seq dataset for EGA table

--- shell/elasticsearch_files/load_ega_runs.bash
+++ shell/elasticsearch_files/load_ega_runs.bash
@@ -8,11 +8,11 @@ HIPSCI_CODE=`dirname $0`/../..
 perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.runs.es.pl \
   $ERA_DB_ARGS \
   -dataset_id EGAD00001000893 \
-  -dataset_id EGAD00001000897 \
+  -dataset_id EGAD00001003529 \
   -dataset_id EGAD00001001422 \
   -dataset_id EGAD00001001437 \
   -dataset_id EGAD00001001438 \
-  -dataset_id EGAD00001001932 \
+  -dataset_id EGAD00001003514 \
   -dataset_id EGAD00001001933 \
   -dataset_id EGAD00001001949 \
   -dataset_id EGAD00001001950 \
@@ -21,7 +21,7 @@ perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.runs.es.pl \
   -dataset_id EGAD00001001953 \
   -dataset_id EGAD00001001954 \
   -dataset_id EGAD00001001955 \
-  -dataset_id EGAD00001003161 \
+  -dataset_id EGAD00001003515 \
   -dataset_id EGAD00001003180 \
   -dataset_id EGAD00001003181 \
   -dataset_id EGAD00001003516 \
@@ -59,6 +59,11 @@ perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.runs.es.pl \
 #  -dataset_id EGAD00001002235 \ new    # Private biosamples, to be checked.
 #  -dataset_id EGAD00001004266 \ new    # Private biosamples, to be checked.
 #  -dataset_id EGAD00001004295 \ new    # Private biosamples, to be checked.
+
+#  -dataset_id EGAD00001003529   replaced EGAD0000100089
+#  -dataset_id EGAD00001003514 needs to replace  EGAD00001001932
+#  -dataset_id EGAD00001003515 needs to replace  EGAD00001003161
+# datasets need to be released.
 
 #  -dataset_id EGAD00001001999 \ to be ignored
 #  -dataset_id EGAD00001002000 \ to be ignored
