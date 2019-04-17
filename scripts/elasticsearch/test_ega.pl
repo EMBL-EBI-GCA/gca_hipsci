9a24b6fbd17fc71c81e482ac82e4a341460f5753
apax18 <amirp@ebi.ac.uk>
2019-04-17 16:21:07 +0100

    test EGA index

--- scripts/elasticsearch/test_ega.pl
+++ scripts/elasticsearch/test_ega.pl
@@ -91,30 +91,38 @@ foreach my $disease (@ReseqTrack::Tools::HipSci::DiseaseParser::diseases) {
     #       'took' => 1
     #     };
     $cohort{donors} = { count => $donor_search->{hits}{total} };
-    print Dumper($cohort{donors});
-}
-#
-#   foreach my $assay (@assays) {
-#
-#     my $search  = $es->call('search',
-#       index => 'hipsci',
-#       type => 'file',
-#       body => {
-#         query => {
-#           constant_score => {
-#             filter => {
-#               bool => {
-#                 must => [
-#                   {term => {'samples.diseaseStatus' => $cohort{disease}{value}}},
-#                   {term => {'assay.type' => $assay}},
-#                   {term => {'archive.name' => 'EGA'}},
-#                 ]
-#               }
-#             }
-#           }
-#         }
-#       }
-#     );
+    # print Dumper($cohort{donors});
+#     $VAR1 = {
+#           'count' => 450
+#         };
+#     $VAR1 = {
+#           'count' => 50
+#         };
+
+
+  foreach my $assay (@assays) {
+
+      my $search = $es->call('search',
+          index => 'hipsci',
+          type  => 'file',
+          body  => {
+              query => {
+                  constant_score => {
+                      filter => {
+                          bool => {
+                              must => [
+                                  { term => { 'samples.diseaseStatus' => $cohort{disease}{value} } },
+                                  { term => { 'assay.type' => $assay } },
+                                  { term => { 'archive.name' => 'EGA' } },
+                              ]
+                          }
+                      }
+                  }
+              }
+          }
+      );
+      print Dumper($search);
+  }
 #     if ($search->{hits}{total}) {
 #       my $accession = $search->{hits}{hits}[0]{_source}{archive}{accession};
 #       push(@{$cohort{datasets}}, {
