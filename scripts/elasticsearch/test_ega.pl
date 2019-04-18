0a635e925756cd9c4a26159defa13857e3b0a999
apax18 <amirp@ebi.ac.uk>
2019-04-18 17:41:56 +0100

    ega dataset

--- scripts/elasticsearch/test_ega.pl
+++ scripts/elasticsearch/test_ega.pl
@@ -122,13 +122,13 @@ foreach my $disease (@ReseqTrack::Tools::HipSci::DiseaseParser::diseases) {
                 }
             }
         );
-        # print Dumper($search);
+        print Dumper($search->{hits}{hits}[0]{_source}{archive}{accession});
         # see below
 
         if ($search->{hits}{total}) {
             # if we there is any
             my $accession = $search->{hits}{hits}[0]{_source}{archive}{accession};
-            print Dumper($accession);
+            # print Dumper($accession);
             push(@{$cohort{datasets}}, {
                 assay         => $assay,
                 archive       => 'EGA',
