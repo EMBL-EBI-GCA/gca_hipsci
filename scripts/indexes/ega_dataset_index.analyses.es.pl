2ce9c72cb25bed770f512ecbd84d11b30fad2c2b
apax18 <amirp@ebi.ac.uk>
2019-04-23 15:50:43 +0100

    ega index

--- scripts/indexes/ega_dataset_index.analyses.es.pl
+++ scripts/indexes/ega_dataset_index.analyses.es.pl
@@ -160,10 +160,10 @@ foreach my $dataset_id (@dataset_id) {
     #         <PRIMARY_ID>ERS1254906</PRIMARY_ID>
 
     my $xml_hash = XMLin($row->{ANALYSIS_XML});
-    print Dumper($xml_hash);
+    # print Dumper($xml_hash);
 
     my $cgap_ips_line = List::Util::first {$_->biosample_id && $_->biosample_id eq $row->{BIOSAMPLE_ID}} @$cgap_ips_lines;
-    # print Dumper($cgap_ips_line);
+    print Dumper($cgap_ips_line);
     my $cgap_tissue = $cgap_ips_line ? $cgap_ips_line->tissue
                     : List::Util::first {$_->biosample_id eq $row->{BIOSAMPLE_ID}} @$cgap_tissues;
     die 'did not recognise sample '.$row->{BIOSAMPLE_ID} if !$cgap_tissue;
