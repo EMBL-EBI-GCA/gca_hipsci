2cfdbf454d155f360a19ff729b02c23e2b76da25
apax18 <amirp@ebi.ac.uk>
2019-10-03 10:30:52 +0100

    test load ega analyses

--- shell/elasticsearch_files/load_ega_analyses.bash
+++ shell/elasticsearch_files/load_ega_analyses.bash
@@ -7,7 +7,6 @@ HIPSCI_CODE=`dirname $0`/../..
 
 perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.analyses.es.pl \
   $ERA_DB_ARGS \
-  -dataset_id EGAD00010001350 \
   -dataset_id EGAD00001003514 \
   -dataset_id EGAD00001003516 \
   -demographic /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv
