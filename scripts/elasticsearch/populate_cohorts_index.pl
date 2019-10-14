810efd6817867fda4d85867b7a8fe7e1caccabaa
apax18 <amirp@ebi.ac.uk>
2019-10-14 14:00:08 +0100

    test removing rare genetic disorder

--- scripts/elasticsearch/populate_cohorts_index.pl
+++ scripts/elasticsearch/populate_cohorts_index.pl
@@ -28,8 +28,8 @@ my $diseases = \@ReseqTrack::Tools::HipSci::DiseaseParser::diseases;
 my $es = ReseqTrack::Tools::HipSci::ElasticsearchClient->new(host => $es_host);
 
 foreach my $disease (@ReseqTrack::Tools::HipSci::DiseaseParser::diseases) {
-  print Dumper($disease->{for_elasticsearch});
   if ($disease->{for_elasticsearch} ne 'Rare genetic neurological disorder') {
+      print Dumper($disease->{for_elasticsearch});
       my %cohort = (
           disease => {
               ontologyPURL => $disease->{ontology_full},
