a611544f1d9e14243efb619b6a7d79ce16136008
apax18 <amirp@ebi.ac.uk>
2020-01-29 14:25:44 +0000

    fixing missing dots

--- shell/elasticsearch_files/load_ega_runs.bash
+++ shell/elasticsearch_files/load_ega_runs.bash
@@ -7,23 +7,30 @@ HIPSCI_CODE=`dirname $0`/../..
 
 perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.runs.es.pl \
   $ERA_DB_ARGS \
-  -dataset_id EGAD00001000893 \
-  -dataset_id EGAD00001000897 \
-  -dataset_id EGAD00001001422 \
-  -dataset_id EGAD00001001437 \
-  -dataset_id EGAD00001001438 \
-  -dataset_id EGAD00001001932 \
-  -dataset_id EGAD00001001933 \
-  -dataset_id EGAD00001001949 \
-  -dataset_id EGAD00001001950 \
-  -dataset_id EGAD00001001951 \
-  -dataset_id EGAD00001001952 \
-  -dataset_id EGAD00001001953 \
-  -dataset_id EGAD00001001954 \
-  -dataset_id EGAD00001001955 \
-  -dataset_id EGAD00001003161 \
-  -dataset_id EGAD00001003180 \
-  -dataset_id EGAD00001003181 \
+  -dataset_id EGAD00010001344 \
+  -dataset_id EGAD00010001346 \
+  -dataset_id EGAD00010001348 \
+  -dataset_id EGAD00010001350 \
+  -dataset_id EGAD00010001352 \
+  -dataset_id EGAD00010001354 \
+  -dataset_id EGAD00010001356 \
+  -dataset_id EGAD00010001358 \
+  -dataset_id EGAD00010001360 \
+  -dataset_id EGAD00010001362 \
+  -dataset_id EGAD00010001364 \
+  -dataset_id EGAD00010001366 \
+  -dataset_id EGAD00010001368 \
+  -dataset_id EGAD00010001370 \
+  -dataset_id EGAD00010001372 \
+  -dataset_id EGAD00010001374 \
+  -dataset_id EGAD00010001376 \
+  -dataset_id EGAD00010001378 \
+  -dataset_id EGAD00010001380 \
+  -dataset_id EGAD00010001382 \
+  -dataset_id EGAD00010001384 \
+  -dataset_id EGAD00010001386 \
+  -dataset_id EGAD00010001388 \
+  -dataset_id EGAD00010001390 \
   -dataset_id EGAD00001003516 \
   -dataset_id EGAD00001003517 \
   -dataset_id EGAD00001003518 \
@@ -56,3 +63,21 @@ perl $HIPSCI_CODE/scripts/indexes/ega_dataset_index.runs.es.pl \
 
 #  -dataset_id EGAD00001001999 \
 #  -dataset_id EGAD00001002000 \
+#  -dataset_id EGAD00001000893 \
+#  -dataset_id EGAD00001000897 \
+#  -dataset_id EGAD00001001422 \
+#  -dataset_id EGAD00001001437 \
+#  -dataset_id EGAD00001001438 \
+#  -dataset_id EGAD00001001932 \
+#  -dataset_id EGAD00001001933 \
+#  -dataset_id EGAD00001001949 \
+#  -dataset_id EGAD00001001950 \
+#  -dataset_id EGAD00001001951 \
+#  -dataset_id EGAD00001001952 \
+#  -dataset_id EGAD00001001953 \
+#  -dataset_id EGAD00001001954 \
+#  -dataset_id EGAD00001001955 \
+#  -dataset_id EGAD00001003161 \
+#  -dataset_id EGAD00001003180 \
+#  -dataset_id EGAD00001003181 \
+
