143252b380702c6af7cb11fa1d5299dbf343637a
apax18 <amirp@ebi.ac.uk>
2020-02-18 17:04:25 +0000

    test

--- shell/elasticsearch_updateserver.bash
+++ shell/elasticsearch_updateserver.bash
@@ -2,6 +2,8 @@
 
 SERVER=ves-hx-e4:9200
 
-perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.qc1_images.pl \
+perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.qc1.pl \
   -es_host=$SERVER \
-  -trim /nfs/hipsci
+  -pluritest /nfs/research1/hipsci/drop/hip-drop/tracked/qc1_raw_data/hipsci.qc1.20171220.pluritest.tsv \
+  -cnv_file /nfs/research1/hipsci/drop/hip-drop/tracked/qc1_raw_data/hipsci.qc1.20171220.cnv_summary.tsv \
+  -cnv_comments_file /nfs/research1/hipsci/drop/hip-drop/tracked/qc1_raw_data/hipsci.qc1.20171220.cnv_comments.tsv
