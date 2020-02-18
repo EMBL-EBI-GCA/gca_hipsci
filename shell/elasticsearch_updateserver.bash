a06198fa0db9f9b5156e8a06f872c9694d9a3f0f
apax18 <amirp@ebi.ac.uk>
2020-02-18 17:00:45 +0000

    test

--- shell/elasticsearch_updateserver.bash
+++ shell/elasticsearch_updateserver.bash
@@ -2,6 +2,6 @@
 
 SERVER=ves-hx-e4:9200
 
-perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.files.pl \
-  -es_host=$SERVER
-
+perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.qc1_images.pl \
+  -es_host=$SERVER \
+  -trim /nfs/hipsci
