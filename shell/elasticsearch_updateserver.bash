70fcbb0952cabfeaff921d1ceb316b5145ef185e
apax18 <amirp@ebi.ac.uk>
2020-02-18 17:12:49 +0000

    test

--- shell/elasticsearch_updateserver.bash
+++ shell/elasticsearch_updateserver.bash
@@ -2,8 +2,7 @@
 
 SERVER=ves-hx-e4:9200
 
-perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.qc1.pl \
+perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.demographic.pl \
   -es_host=$SERVER \
-  -pluritest /nfs/research1/hipsci/drop/hip-drop/tracked/qc1_raw_data/hipsci.qc1.20171220.pluritest.tsv \
-  -cnv_file /nfs/research1/hipsci/drop/hip-drop/tracked/qc1_raw_data/hipsci.qc1.20171220.cnv_summary.tsv \
-  -cnv_comments_file /nfs/research1/hipsci/drop/hip-drop/tracked/qc1_raw_data/hipsci.qc1.20171220.cnv_comments.tsv
+  -demographic_file /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv \
+  -sex_sequenome_file /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/sex_sequenome.20160729.tsv
