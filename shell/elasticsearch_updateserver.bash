c6f7376214f8c39a86568b1ea5fdb5de785e4cdb
apax18 <amirp@ebi.ac.uk>
2020-02-18 17:16:49 +0000

    test

--- shell/elasticsearch_updateserver.bash
+++ shell/elasticsearch_updateserver.bash
@@ -2,7 +2,5 @@
 
 SERVER=ves-hx-e4:9200
 
-perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.demographic.pl \
-  -es_host=$SERVER \
-  -demographic_file /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/Demographicdata_HipSci_2016-12-02.csv \
-  -sex_sequenome_file /nfs/research1/hipsci/tracking_resources/demographic_spreadsheets/sex_sequenome.20160729.tsv
+perl $HIPSCI_CODE/scripts/elasticsearch/populate_fibroblast_line.from_files_elasticsearch.pl \
+  -es_host=$SERVER
