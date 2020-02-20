57dc593e06a1f11b0d26b4156cf8f69f48e56621
apax18 <amirp@ebi.ac.uk>
2020-02-20 13:48:39 +0000

    test credentials

--- shell/elasticsearch_updateserver.bash
+++ shell/elasticsearch_updateserver.bash
@@ -5,5 +5,6 @@ SERVER=ves-hx-e4:9200
 perl $HIPSCI_CODE/scripts/elasticsearch/populate_cell_line.from_cgap_report.pl \
   -es_host=$SERVER \
   -ecacc_index_file $HIPSCI_CODE/tracking_resources/ecacc_catalog_numbers.tsv \
+  -hESCreg_user $HESCREG_USER \
   -hESCreg_pass $HESCREG_PASS \
-  -hESCreg_user $HESCREG_USER
+  --predicted_population_filename=/nfs/research1/hipsci/drop/hip-drop/tracked/predicted_population/hipsci.pca_557.20170928.predicted_populations.tsv
