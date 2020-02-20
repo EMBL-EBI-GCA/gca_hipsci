8f30002c2b4df9988fced7774871fc479d1f6b7a
apax18 <amirp@ebi.ac.uk>
2020-02-20 10:15:32 +0000

    fixing dots

--- shell/elasticsearch_updateserver.bash
+++ shell/elasticsearch_updateserver.bash
@@ -2,5 +2,7 @@
 
 SERVER=ves-hx-e4:9200
 
-perl $HIPSCI_CODE/scripts/elasticsearch/populate_fibroblast_line.from_files_elasticsearch.pl \
-  -es_host=$SERVER
+perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.differentiations.pl \
+  -es_host=$SERVER \
+  --yaml=$HIPSCI_CODE/tracking_resources/differentiations/macrophage_2016.yaml \
+  --yaml=$HIPSCI_CODE/tracking_resources/differentiations/sensory_neurons_2016.yaml \
