47d65219563727db40f676b554cdca2eea033c45
apax18 <amirp@ebi.ac.uk>
2020-02-20 10:23:46 +0000

    fixing dots

--- shell/elasticsearch_updateserver.bash
+++ shell/elasticsearch_updateserver.bash
@@ -2,7 +2,8 @@
 
 SERVER=ves-hx-e4:9200
 
-perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.predicted_populations.pl \
+perl $HIPSCI_CODE/scripts/elasticsearch/populate_cell_line.from_cgap_report.pl \
   -es_host=$SERVER \
-  --predicted_population_filename=/nfs/research1/hipsci/drop/hip-drop/tracked/predicted_population/hipsci.pca_557.20170928.predicted_populations.tsv
-
+  -ecacc_index_file $HIPSCI_CODE/tracking_resources/ecacc_catalog_numbers.tsv \
+  -hESCreg_user $HESCREG_USER \
+  -hESCreg_pass $HESCREG_PASS \
