f675a38d9d9f347041771321019cbe179b44e909
apax18 <amirp@ebi.ac.uk>
2020-02-20 13:57:33 +0000

    test credentials

--- shell/elasticsearch_updateserver.bash
+++ shell/elasticsearch_updateserver.bash
@@ -5,5 +5,5 @@ SERVER=ves-hx-e4:9200
 perl $HIPSCI_CODE/scripts/elasticsearch/populate_cell_line.from_cgap_report.pl \
   -es_host=$SERVER \
   -ecacc_index_file $HIPSCI_CODE/tracking_resources/ecacc_catalog_numbers.tsv \
-  -hESCreg_user $HESCREG_USER \
-  -hESCreg_pass $HESCREG_PASS
+  -hESCreg_user HESCREG_USER \
+  -hESCreg_pass HESCREG_PASS
