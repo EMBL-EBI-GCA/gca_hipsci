43624d505eccc333d3f0463051d971d37f443512
apax18 <amirp@ebi.ac.uk>
2020-03-05 14:02:23 +0000

    fix dots

--- shell/elasticsearch_updateserver.bash
+++ shell/elasticsearch_updateserver.bash
@@ -2,5 +2,5 @@
 
 SERVER=ves-hx-e4:9200
 
-&& perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.files.pl \
+perl $HIPSCI_CODE/scripts/elasticsearch/update_cell_line.files.pl \
   -es_host=$SERVER
